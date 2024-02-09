b93195dd26767e1d0e689c8d2825a8bfe0992d52
Ivan Dagelic <dagelic.ivan@gmail.com>
2024-02-09 12:55:11 +0100

    remote server configuration

--- server/grpc/server/get_public_key.go
+++ server/grpc/server/get_public_key.go
@@ -8,9 +8,10 @@ import (
 
 	daytona_proto "github.com/daytonaio/daytona/common/grpc/proto"
 	config_ssh_key "github.com/daytonaio/daytona/server/config/ssh_key"
+	"github.com/golang/protobuf/ptypes/empty"
 )
 
-func (a *ServerGRPCServer) GetPublicKey(ctx context.Context, request *daytona_proto.GetPublicKeyRequest) (*daytona_proto.GetPublicKeyResponse, error) {
+func (a *ServerGRPCServer) GetPublicKey(ctx context.Context, request *empty.Empty) (*daytona_proto.GetPublicKeyResponse, error) {
 	publicKey, err := config_ssh_key.GetPublicKey()
 	if err != nil {
 		return nil, err
