181138978822b783bff4d250a2065b9ec17fbfc1
Ivan Dagelic <dagelic.ivan@gmail.com>
2024-02-23 18:18:31 +0100

    chore remove unused function

--- pkg/git_provider/git_provider.go
+++ pkg/git_provider/git_provider.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 
 	"github.com/daytonaio/daytona/cli/config"
-	"github.com/daytonaio/daytona/common/api_client"
 	"github.com/daytonaio/daytona/common/types"
 )
 
@@ -32,37 +31,6 @@ type GitRepository struct {
 	FullName string
 	Name     string
 	Url      string
-}
-
-func GetGitProviderFront(providerId string, gitProviders []api_client.GitProvider) (GitProvider, error) {
-	var chosenProvider *api_client.GitProvider
-	for _, gitProvider := range gitProviders {
-		if *gitProvider.Id == providerId {
-			chosenProvider = &gitProvider
-		}
-	}
-
-	if chosenProvider == nil {
-		return nil, errors.New("provider not found")
-	}
-
-	switch providerId {
-	case "github":
-		return &GitHubGitProvider{
-			token: *chosenProvider.Token,
-		}, nil
-	case "gitlab":
-		return &GitLabGitProvider{
-			token: *chosenProvider.Token,
-		}, nil
-	case "bitbucket":
-		return &BitbucketGitProvider{
-			username: *chosenProvider.Username,
-			token:    *chosenProvider.Token,
-		}, nil
-	default:
-		return nil, errors.New("provider not found")
-	}
 }
 
 func GetGitProvider(providerId string, gitProviders []types.GitProvider) GitProvider {
