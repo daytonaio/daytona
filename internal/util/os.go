99d56ff68d3659eeb188a1a9c7dada486fdd837a
Ivan Dagelic <dagelic.ivan@gmail.com>
2024-02-26 12:19:06 +0100

    windows client

--- internal/util/os.go
+++ internal/util/os.go
@@ -2,11 +2,16 @@ package util
 
 import (
 	"os/exec"
+	"runtime"
 
 	"github.com/daytonaio/daytona/common/os"
 )
 
 func GetOperatingSystem() (*os.OperatingSystem, error) {
+	if runtime.GOOS == "windows" {
+		return GetOperatingSystemWindows()
+	}
+
 	cmd := exec.Command("uname", "-a")
 	output, err := cmd.Output()
 	if err != nil {
@@ -14,4 +19,13 @@ func GetOperatingSystem() (*os.OperatingSystem, error) {
 	}
 
 	return os.OSFromUnameA(string(output))
+}
+
+func GetOperatingSystemWindows() (*os.OperatingSystem, error) {
+	cmd := exec.Command("systeminfo")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	return os.OSFromEchoProcessor(string(output))
 }
