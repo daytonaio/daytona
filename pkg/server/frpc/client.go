27100a99f9933017cfc58ebd1618d0b844b4902f
Tpuljak <toma.puljak@hotmail.com>
2024-03-03 15:06:08 +0000

    feat: port forwarding

--- pkg/server/frpc/client.go
+++ pkg/server/frpc/client.go
@@ -4,13 +4,11 @@
 package frpc
 
 import (
-	"context"
 	"fmt"
 
+	"github.com/daytonaio/daytona/pkg/frpc"
 	"github.com/daytonaio/daytona/pkg/server/config"
 	"github.com/daytonaio/daytona/pkg/types"
-	"github.com/fatedier/frp/client"
-	v1 "github.com/fatedier/frp/pkg/config/v1"
 )
 
 func ConnectServer() error {
@@ -19,53 +17,28 @@ func ConnectServer() error {
 		return err
 	}
 
-	cfg := client.ServiceOptions{}
-	cfg.Common = &v1.ClientCommonConfig{}
-	cfg.Common.ServerAddr = c.Frps.Domain
-	cfg.Common.ServerPort = int(c.Frps.Port)
-	cfg.ProxyCfgs = []v1.ProxyConfigurer{}
-
-	httpConfig := &v1.HTTPProxyConfig{}
-	httpConfig.GetBaseConfig().Name = fmt.Sprintf("daytona-server-%s", c.Id)
-	httpConfig.GetBaseConfig().LocalPort = int(c.HeadscalePort)
-	httpConfig.GetBaseConfig().Type = string(v1.ProxyTypeHTTP)
-	httpConfig.SubDomain = c.Id
-
-	cfg.ProxyCfgs = append(cfg.ProxyCfgs, httpConfig)
-
-	service, err := client.NewService(cfg)
-	if err != nil {
-		return err
-	}
-
-	return service.Run(context.Background())
+	return frpc.Connect(frpc.FrpcConnectParams{
+		ServerDomain: c.Frps.Domain,
+		ServerPort:   int(c.Frps.Port),
+		Name:         fmt.Sprintf("daytona-server-%s", c.Id),
+		Port:         int(c.HeadscalePort),
+		SubDomain:    c.Id,
+	})
 }
+
 func ConnectApi() error {
 	c, err := config.GetConfig()
 	if err != nil {
 		return err
 	}
 
-	cfg := client.ServiceOptions{}
-	cfg.Common = &v1.ClientCommonConfig{}
-	cfg.Common.ServerAddr = c.Frps.Domain
-	cfg.Common.ServerPort = int(c.Frps.Port)
-	cfg.ProxyCfgs = []v1.ProxyConfigurer{}
-
-	httpConfig := &v1.HTTPProxyConfig{}
-	httpConfig.GetBaseConfig().Name = fmt.Sprintf("daytona-server-api-%s", c.Id)
-	httpConfig.GetBaseConfig().LocalPort = int(c.ApiPort)
-	httpConfig.GetBaseConfig().Type = string(v1.ProxyTypeHTTP)
-	httpConfig.SubDomain = fmt.Sprintf("api-%s", c.Id)
-
-	cfg.ProxyCfgs = append(cfg.ProxyCfgs, httpConfig)
-
-	service, err := client.NewService(cfg)
-	if err != nil {
-		return err
-	}
-
-	return service.Run(context.Background())
+	return frpc.Connect(frpc.FrpcConnectParams{
+		ServerDomain: c.Frps.Domain,
+		ServerPort:   int(c.Frps.Port),
+		Name:         fmt.Sprintf("daytona-server-api-%s", c.Id),
+		Port:         int(c.ApiPort),
+		SubDomain:    fmt.Sprintf("api-%s", c.Id),
+	})
 }
 
 func GetApiDomain(c *types.ServerConfig) string {
