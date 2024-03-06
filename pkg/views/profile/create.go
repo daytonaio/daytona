95c18360818b509e6f8017f46c5ae3478e590c75
Tpuljak <toma.puljak@hotmail.com>
2024-03-06 10:04:03 +0000

    refactor: remove remote installer pkg

--- pkg/views/profile/create.go
+++ pkg/views/profile/create.go
@@ -7,39 +7,20 @@ import (
 	"errors"
 	"log"
 	"regexp"
-	"strconv"
 	"strings"
 
 	"github.com/daytonaio/daytona/cmd/daytona/config"
 	"github.com/daytonaio/daytona/pkg/views"
-	"github.com/daytonaio/daytona/pkg/views/util"
 
 	"github.com/charmbracelet/huh"
 )
 
 type ProfileAddView struct {
-	ProfileName             string
-	RemoteHostname          string
-	RemoteSshPort           int
-	RemoteSshPassword       string
-	RemoteSshUser           string
-	RemoteSshPrivateKeyPath string
-	ApiUrl                  string
+	ProfileName string
+	ApiUrl      string
 }
 
-func ProfileCreationView(c *config.Config, profileAddView *ProfileAddView, editing, forceRemoteAccess, skipName bool) {
-	var authType util.SshAuthType
-
-	var portString string = strconv.Itoa(profileAddView.RemoteSshPort)
-
-	hasAccessToRemote := forceRemoteAccess
-
-	if editing {
-		hasAccessToRemote = true
-	}
-
-	var nameGroup *huh.Group
-
+func ProfileCreationView(c *config.Config, profileAddView *ProfileAddView, editing bool) {
 	nameInput := huh.NewInput().
 		Title("Profile name").
 		Validate(func(str string) error {
@@ -62,51 +43,19 @@ func ProfileCreationView(c *config.Config, profileAddView *ProfileAddView, editi
 		}).
 		Value(&profileAddView.ProfileName)
 
-	remoteAccessConfirm := huh.NewConfirm().
-		Title("Do you have direct access to the machine running the Daytona Server?").
-		Description("If you have direct access, you will be asked to input your SSH credentials.\nIf not, you will only have to provide the API URL of the Daytona Server.").
-		Value(&hasAccessToRemote)
-
-	if forceRemoteAccess || editing {
-		nameGroup = huh.NewGroup(
-			nameInput,
-		).WithHideFunc(func() bool {
-			return skipName
-		})
-	} else {
-		nameGroup = huh.NewGroup(
-			nameInput,
-			remoteAccessConfirm,
-		).WithHideFunc(func() bool {
-			return skipName
-		})
-	}
-
-	formGroups := []*huh.Group{
-		nameGroup,
+	form := huh.NewForm(
 		huh.NewGroup(
+			nameInput,
 			huh.NewInput().
 				Title("Server API URL").
-				Value(&profileAddView.ApiUrl),
-		).WithHideFunc(func() bool {
-			if forceRemoteAccess {
-				return true
-			}
-
-			if editing {
-				return false
-			}
-
-			return hasAccessToRemote
-		}),
-	}
-
-	remoteAuthGroups := remoteAuthGroups(profileAddView, &authType, &portString, &hasAccessToRemote)
-
-	formGroups = append(formGroups, remoteAuthGroups...)
-
-	form := huh.NewForm(
-		formGroups...,
+				Value(&profileAddView.ApiUrl).
+				Validate(func(str string) error {
+					if str == "" {
+						return errors.New("server API URL can not be blank")
+					}
+					return nil
+				}),
+		),
 	).WithTheme(views.GetCustomTheme())
 
 	err := form.Run()
@@ -114,76 +63,7 @@ func ProfileCreationView(c *config.Config, profileAddView *ProfileAddView, editi
 		log.Fatal(err)
 	}
 
-	profileAddView.RemoteSshPort, err = strconv.Atoi(portString)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func remoteAuthGroups(profileAddView *ProfileAddView, authType *util.SshAuthType, portString *string, hasAccessToRemote *bool) []*huh.Group {
-	privateKeyGroups := util.PrivateKeyForm(authType, &profileAddView.RemoteSshUser, &profileAddView.RemoteSshPrivateKeyPath)
-
-	res := []*huh.Group{
-		huh.NewGroup(
-			huh.NewInput().
-				Title("Remote SSH hostname").
-				Value(&profileAddView.RemoteHostname).
-				Validate(func(str string) error {
-					if str == "" {
-						return errors.New("hostname can not be blank")
-					}
-					return nil
-				}),
-			huh.NewInput().
-				Title("Remote SSH port").
-				Value(portString).
-				Validate(func(str string) error {
-					if str == "" {
-						return errors.New("port can not be blank")
-					}
-
-					if _, err := strconv.Atoi(str); err != nil {
-						return errors.New("port must be a number")
-					}
-
-					return nil
-				}),
-			huh.NewSelect[string]().
-				Title("Authentication type").
-				Options(
-					huh.NewOption("Password", util.SshAuthTypePassword),
-					huh.NewOption("Private key", util.SshAuthTypePrivateKey),
-				).
-				Value(authType),
-		).WithHideFunc(func() bool {
-			return !*hasAccessToRemote
-		}),
-		huh.NewGroup(
-			huh.NewInput().
-				Title("Remote SSH user").
-				Value(&profileAddView.RemoteSshUser).
-				Validate(func(str string) error {
-					if str == "" {
-						return errors.New("user can not be blank")
-					}
-					return nil
-				}),
-			huh.NewInput().
-				Title("Remote SSH password").
-				Value(&profileAddView.RemoteSshPassword).
-				Password(true).
-				Validate(func(str string) error {
-					if str == "" {
-						return errors.New("password can not be blank")
-					}
-					return nil
-				}),
-		).WithHideFunc(func() bool {
-			return *authType != "password" || !*hasAccessToRemote
-		}),
-	}
-
-	res = append(res, privateKeyGroups...)
-
-	return res
 }
