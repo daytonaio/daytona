4849cf939ea636ac4b4274a240e8ce72b84ee8aa
Ivan Dagelic <dagelic.ivan@gmail.com>
2024-03-11 12:13:47 +0100

    fix: single project create

--- pkg/views/workspace/create/view.go
+++ pkg/views/workspace/create/view.go
@@ -114,7 +114,7 @@ func runInitialForm(providerRepo types.Repository, multiProject bool) (Workspace
 	m.form = huh.NewForm(
 		huh.NewGroup(
 			primaryRepoPrompt,
-		).WithHide(!multiProject),
+		).WithHide(primaryRepoUrl != ""),
 		huh.NewGroup(
 			secondaryProjectCountPrompt,
 		).WithHide(!multiProject),
