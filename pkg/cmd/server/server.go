c52f5a9c260f2fe82035ad1649b030e6bdb81303
Toma Puljak <toma.puljak@hotmail.com>
2024-03-14 17:52:02 +0100

    feat: server started message (#211)
    Show the user how to connect to the server remotely when the server is started.
    
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- pkg/cmd/server/server.go
+++ pkg/cmd/server/server.go
@@ -4,8 +4,13 @@
 package server
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/daytonaio/daytona/pkg/server"
 	"github.com/daytonaio/daytona/pkg/server/config"
+	"github.com/daytonaio/daytona/pkg/server/frpc"
+	"github.com/daytonaio/daytona/pkg/views/util"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -20,17 +25,27 @@ var ServerCmd = &cobra.Command{
 			//	for now, force the log level to info when running the server
 			log.SetLevel(log.InfoLevel)
 		}
+		errCh := make(chan error)
 
-		config, err := config.GetConfig()
+		err := server.Start(errCh)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if config == nil {
-			log.Fatal("Server configuration is not set. Please run `daytona configure`.")
+		c, err := config.GetConfig()
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		err = server.Start()
+		select {
+		case err := <-errCh:
+			log.Fatal(err)
+		// TODO: This is an optimistic check. We should check if the server is actually running
+		case <-time.After(5 * time.Second):
+			util.RenderBorderedMessage(fmt.Sprintf("Daytona Server running on port: %d.\nTo connect to the server remotely, use the following command on the client machine:\n\ndaytona profile add -a %s", c.ApiPort, frpc.GetApiUrl(c)))
+		}
+
+		err = <-errCh
 		if err != nil {
 			log.Fatal(err)
 		}
