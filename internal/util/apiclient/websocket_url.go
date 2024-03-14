e21bc3c0e6dd5d3cff4e02b23abf11911e6cd5e9
Toma Puljak <toma.puljak@hotmail.com>
2024-03-14 16:46:27 +0000

    fix: use wss for https API URLs
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

---
+++ internal/util/apiclient/websocket_url.go
@@ -0,0 +1,26 @@
+package apiclient
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+)
+
+func GetWebSocketUrl(apiUrl string) (string, error) {
+	hostRegex := regexp.MustCompile(`(https*)://(.*)`)
+
+	matches := hostRegex.FindStringSubmatch(apiUrl)
+
+	if len(matches) != 3 {
+		return "", errors.New("invalid API URL")
+	}
+
+	switch matches[1] {
+	case "http":
+		return fmt.Sprintf("ws://%s", matches[2]), nil
+	case "https":
+		return fmt.Sprintf("wss://%s", matches[2]), nil
+	}
+
+	return "", errors.New("invalid API URL protocol")
+}
