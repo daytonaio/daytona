bbbdd100a7bb919e2a08021c8028f31518b79118
Chad Metcalf <metcalfc@gmail.com>
2024-03-13 20:11:22 -0700

    Move to prebuilt devcontainer.
    Addresses #201.
    
    Signed-off-by: Chad Metcalf <metcalfc@gmail.com>

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,10 +1,6 @@
-npm install -g @devcontainers/cli
+#!/bin/bash
 
-go get -u google.golang.org/protobuf/cmd/protoc-gen-go
-go install google.golang.org/protobuf/cmd/protoc-gen-go
-
-go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
+go install github.com/swaggo/swag/cmd/swag@v1.16.3
 
 go mod tidy
 
