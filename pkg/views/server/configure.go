24c01180c94349fa9ef16451f372f1d4e78008f8
Luo Wenhao <xiaot.qaq@gmail.com>
2024-03-22 02:32:18 +0800

    feat: allow server defaults override via cli
    Signed-off-by: Luo Wenhao <xiaot.qaq@gmail.com>

--- pkg/views/server/configure.go
+++ pkg/views/server/configure.go
@@ -4,8 +4,10 @@
 package server
 
 import (
+	"errors"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/charmbracelet/huh"
 	"github.com/daytonaio/daytona/pkg/serverapiclient"
@@ -17,6 +19,10 @@ type ServerUpdateKeyView struct {
 }
 
 func ConfigurationForm(config *serverapiclient.ServerConfig) *serverapiclient.ServerConfig {
+	apiPort := strconv.Itoa(int(config.GetApiPort()))
+	headscalePort := strconv.Itoa(int(config.GetHeadscalePort()))
+	frpsPort := strconv.Itoa(int(config.Frps.GetPort()))
+
 	form := huh.NewForm(
 		huh.NewGroup(
 			huh.NewInput().
@@ -36,6 +42,26 @@ func ConfigurationForm(config *serverapiclient.ServerConfig) *serverapiclient.Se
 			huh.NewInput().
 				Title("Server Download URL").
 				Value(config.ServerDownloadUrl),
+			huh.NewInput().
+				Title("API Port").
+				Value(&apiPort).
+				Validate(createPortValidator(config, &apiPort, "api")),
+			huh.NewInput().
+				Title("Headscale Port").
+				Value(&headscalePort).
+				Validate(createPortValidator(config, &headscalePort, "headscale")),
+		),
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Frps Domain").
+				Value(config.Frps.Domain),
+			huh.NewInput().
+				Title("Frps Port").
+				Value(&frpsPort).
+				Validate(createPortValidator(config, &frpsPort, "frps")),
+			huh.NewInput().
+				Title("Frps Protocol").
+				Value(config.Frps.Protocol),
 		),
 	)
 
@@ -45,4 +71,32 @@ func ConfigurationForm(config *serverapiclient.ServerConfig) *serverapiclient.Se
 	}
 
 	return config
+}
+
+func createPortValidator(config *serverapiclient.ServerConfig, s *string, field string) func(string) error {
+	return func(string) error {
+		port, err := strconv.Atoi(*s)
+		if err != nil {
+			return errors.New("failed to parse port")
+		}
+		if port < 0 || port > 65535 {
+			return errors.New("port out of range")
+		}
+		validatedPort := int32(port)
+
+		switch field {
+		case "api":
+			config.ApiPort = &validatedPort
+		case "headscale":
+			config.HeadscalePort = &validatedPort
+		case "frps":
+			config.Frps.Port = &validatedPort
+		}
+
+		if *config.ApiPort == *config.HeadscalePort {
+			return errors.New("port conflict")
+		}
+
+		return nil
+	}
 }
