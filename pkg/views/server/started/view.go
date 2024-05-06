0b6c5cb8389149303e1066bc528a85e09c7b9745
Ivan Dagelić <dagelic.ivan@gmail.com>
2024-05-06 16:49:24 +0200

    fix: remove server border (#477)
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- pkg/views/server/started/view.go
+++ pkg/views/server/started/view.go
@@ -5,11 +5,9 @@ package started
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/daytonaio/daytona/pkg/views"
-	"golang.org/x/term"
 )
 
 func Render(apiPort uint32, frpcUrl string, isDaemonMode bool) {
@@ -20,19 +18,7 @@ func Render(apiPort uint32, frpcUrl string, isDaemonMode bool) {
 	output += "You may now begin developing locally"
 	output += "\n"
 
-	terminalWidth, _, err := term.GetSize(int(os.Stdout.Fd()))
-	if err != nil {
-		fmt.Println(output)
-		return
-	}
-
-	output = lipgloss.NewStyle().PaddingLeft(3).Render(output)
-
-	output = lipgloss.
-		NewStyle().
-		BorderForeground(views.LightGray).
-		Border(lipgloss.RoundedBorder()).Width(views.GetContainerBreakpointWidth(terminalWidth)).
-		Render(output) + "\n"
+	output = lipgloss.NewStyle().PaddingLeft(3).Render(output) + "\n"
 
 	if !isDaemonMode {
 		output = "\n" + output
