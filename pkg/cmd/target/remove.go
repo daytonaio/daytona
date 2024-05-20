c20ac6889695c60ed0a278e2341de204280bb0b0
mobley-trent <oyieko99@gmail.com>
2024-05-20 19:49:40 +0300

    Updated pkg/cmd/target/remove.go: Forceful removal of all workspaces upon target deletion
    Signed-off-by: mobley-trent <oyieko99@gmail.com>

--- pkg/cmd/target/remove.go
+++ pkg/cmd/target/remove.go
@@ -50,14 +50,38 @@ var targetRemoveCmd = &cobra.Command{
 			selectedTargetName = args[0]
 		}
 
+		ctx := context.Background()
 		client, err := apiclient.GetApiClient(nil)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		res, err := client.TargetAPI.RemoveTarget(context.Background(), selectedTargetName).Execute()
+		res, err := client.TargetAPI.RemoveTarget(ctx, selectedTargetName).Execute()
 		if err != nil {
 			log.Fatal(apiclient.HandleErrorResponse(res, err))
+		}
+
+		workspaceList, res, err := client.WorkspaceAPI.ListWorkspaces(ctx).Execute()
+		if err != nil {
+			log.Error(apiclient.HandleErrorResponse(res, err))
+		}
+
+		if len(workspaceList) > 0 {
+			views.RenderInfoMessage(fmt.Sprintln("Deleting workspaces within target..."))
+			for _, workspace := range workspaceList {
+				if *workspace.Target != selectedTargetName {
+					continue
+				}
+
+				res, err := client.WorkspaceAPI.RemoveWorkspace(ctx, *workspace.Id).Execute()
+				if err != nil {
+					log.Errorf("Failed to delete workspace %s: %v", *workspace.Name, apiclient.HandleErrorResponse(res, err))
+					continue
+				}
+
+				views.RenderLine(fmt.Sprintf("- Workspace %s successfully deleted\n", *workspace.Name))
+
+			}
 		}
 
 		views.RenderInfoMessageBold(fmt.Sprintf("Target %s removed successfully", selectedTargetName))
