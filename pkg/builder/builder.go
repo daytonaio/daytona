9a239c383dd44deea5c4f57535ca6822ede2ac26
Vedran <vedran.jukic@gmail.com>
2024-05-22 15:39:56 +0000

    refactor: builder abstract struct
    Signed-off-by: Vedran <vedran.jukic@gmail.com>

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -8,18 +8,10 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/daytonaio/daytona/pkg/containerregistry"
-	"github.com/daytonaio/daytona/pkg/git"
 	"github.com/daytonaio/daytona/pkg/gitprovider"
 	"github.com/daytonaio/daytona/pkg/logger"
 	"github.com/daytonaio/daytona/pkg/workspace"
-	"github.com/docker/docker/pkg/stringid"
-	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
-
-type Catalog struct {
-	Repositories []string `json:"repositories"`
-}
 
 type BuildResult struct {
 	User               string
@@ -27,12 +19,6 @@ type BuildResult struct {
 	ProjectVolumePath  string
 	PostCreateCommands []string
 	PostStartCommands  []string
-}
-
-type BuilderPlugin interface {
-	Build() (*BuildResult, error)
-	Publish() error
-	CleanUp() error
 }
 
 type BuilderConfig struct {
@@ -46,19 +32,18 @@ type BuilderConfig struct {
 }
 
 type IBuilder interface {
-	Prepare() error
-	LoadBuildResults() (*BuildResult, error)
+	Build() (*BuildResult, error)
+	CleanUp() error
+	Publish() error
 	SaveBuildResults(r BuildResult) error
-	GetBuilderPlugin() BuilderPlugin
 }
 
 type Builder struct {
 	id                string
-	plugin            BuilderPlugin
 	project           workspace.Project
-	containerRegistry *containerregistry.ContainerRegistry
 	gitProviderConfig *gitprovider.GitProviderConfig
 	hash              string
+	projectVolumePath string
 
 	daytonaServerConfigFolder       string
 	localContainerRegistryServer    string
@@ -67,178 +52,6 @@ type Builder struct {
 	defaultProjectImage             string
 	defaultProjectUser              string
 	defaultProjectPostStartCommands []string
-}
-
-type IBuilderFactory interface {
-	Create(p workspace.Project, cr *containerregistry.ContainerRegistry, gpc *gitprovider.GitProviderConfig) IBuilder
-}
-
-type BuilderFactory struct {
-	daytonaServerConfigFolder       string
-	localContainerRegistryServer    string
-	basePath                        string
-	loggerFactory                   logger.LoggerFactory
-	defaultProjectImage             string
-	defaultProjectUser              string
-	defaultProjectPostStartCommands []string
-}
-
-func NewBuilderFactory(config BuilderConfig) IBuilderFactory {
-	return &BuilderFactory{
-		daytonaServerConfigFolder:       config.DaytonaServerConfigFolder,
-		localContainerRegistryServer:    config.LocalContainerRegistryServer,
-		basePath:                        config.BasePath,
-		loggerFactory:                   config.LoggerFactory,
-		defaultProjectImage:             config.DefaultProjectImage,
-		defaultProjectUser:              config.DefaultProjectUser,
-		defaultProjectPostStartCommands: config.DefaultProjectPostStartCommands,
-	}
-}
-
-func (f *BuilderFactory) Create(p workspace.Project, cr *containerregistry.ContainerRegistry, gpc *gitprovider.GitProviderConfig) IBuilder {
-	buildId := stringid.GenerateRandomID()
-	buildId = stringid.TruncateID(buildId)
-
-	builder := &Builder{
-		id:                buildId,
-		plugin:            nil,
-		project:           p,
-		containerRegistry: cr,
-		gitProviderConfig: gpc,
-
-		daytonaServerConfigFolder:       f.daytonaServerConfigFolder,
-		localContainerRegistryServer:    f.localContainerRegistryServer,
-		basePath:                        f.basePath,
-		loggerFactory:                   f.loggerFactory,
-		defaultProjectImage:             f.defaultProjectImage,
-		defaultProjectUser:              f.defaultProjectUser,
-		defaultProjectPostStartCommands: f.defaultProjectPostStartCommands,
-	}
-
-	return builder
-}
-
-func (b *Builder) GetBuilderPlugin() BuilderPlugin {
-	return b.plugin
-}
-
-func (b *Builder) Prepare() error {
-	hash, err := b.project.GetConfigHash()
-	if err != nil {
-		return err
-	}
-	b.hash = hash
-	projectDir := filepath.Join(b.basePath, hash, "project")
-
-	err = os.RemoveAll(projectDir)
-	if err != nil {
-		return err
-	}
-
-	projectLogger := b.loggerFactory.CreateProjectLogger(b.project.WorkspaceId, b.project.Name)
-	defer projectLogger.Close()
-
-	gitservice := git.Service{
-		ProjectDir:        projectDir,
-		GitConfigFileName: "",
-		LogWriter:         projectLogger,
-	}
-
-	var auth *http.BasicAuth
-	if b.gitProviderConfig != nil {
-		auth = &http.BasicAuth{
-			Username: b.gitProviderConfig.Username,
-			Password: b.gitProviderConfig.Token,
-		}
-	}
-
-	err = gitservice.CloneRepository(&b.project, auth)
-	if err != nil {
-		return err
-	}
-
-	buildConfig := b.project.Build
-
-	if buildConfig != nil && *buildConfig == (workspace.ProjectBuild{}) {
-		//	detect is devcontainer
-		devcontainerPath := ".devcontainer/devcontainer.json"
-		isDevcontainer, err := fileExists(filepath.Join(projectDir, devcontainerPath))
-		if err != nil {
-			return err
-		}
-		if !isDevcontainer {
-			devcontainerPath = ".devcontainer.json"
-			isDevcontainer, err = fileExists(filepath.Join(projectDir, devcontainerPath))
-			if err != nil {
-				return err
-			}
-		}
-		if isDevcontainer {
-			buildConfig.Devcontainer = &workspace.ProjectBuildDevcontainer{
-				DevContainerFilePath: devcontainerPath,
-			}
-			goto initPlugin
-		}
-		//	todo: detect dockerfile
-		//	todo: detect nix
-
-		//	no supported dev config standard found
-		//	set default project image to ensure that project will run anyway
-		b.project.Image = b.defaultProjectImage
-		b.project.User = b.defaultProjectUser
-		b.project.PostStartCommands = b.defaultProjectPostStartCommands
-	}
-
-initPlugin:
-
-	if buildConfig.Devcontainer != nil {
-		b.plugin = &DevcontainerBuilder{
-			DevcontainerBuilderConfig: DevcontainerBuilderConfig{
-				buildId:                      b.id,
-				project:                      b.project,
-				loggerFactory:                b.loggerFactory,
-				localContainerRegistryServer: b.localContainerRegistryServer,
-				projectVolumePath:            filepath.Join(b.basePath, b.hash, "project"),
-			},
-		}
-	}
-
-	return nil
-}
-
-func (b *Builder) LoadBuildResults() (*BuildResult, error) {
-	hash, err := b.project.GetConfigHash()
-	if err != nil {
-		return nil, err
-	}
-
-	filePath := filepath.Join(b.daytonaServerConfigFolder, "builds", hash, "build.json")
-
-	_, err = os.Stat(filePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil, nil
-		}
-		if pathErr, ok := err.(*os.PathError); ok && pathErr.Err.Error() == "not a directory" {
-			return nil, nil
-		}
-		return nil, err
-	}
-
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var result BuildResult
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&result)
-	if err != nil {
-		return nil, err
-	}
-
-	return &result, nil
 }
 
 func (b *Builder) SaveBuildResults(r BuildResult) error {
@@ -260,15 +73,4 @@ func (b *Builder) SaveBuildResults(r BuildResult) error {
 	}
 
 	return nil
-}
-
-func fileExists(filePath string) (bool, error) {
-	_, err := os.Stat(filePath)
-	if os.IsNotExist(err) {
-		return false, nil
-	} else if err != nil {
-		// There was an error checking for the file
-		return false, err
-	}
-	return true, nil
 }
--- pkg/builder/devcontainer.go
+++ pkg/builder/devcontainer.go
@@ -18,8 +18,6 @@ import (
 
 	"github.com/daytonaio/daytona/pkg/builder/devcontainer"
 	"github.com/daytonaio/daytona/pkg/docker"
-	"github.com/daytonaio/daytona/pkg/logger"
-	"github.com/daytonaio/daytona/pkg/workspace"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/image"
@@ -33,17 +31,8 @@ type BuildOutcome struct {
 	RemoteWorkspaceFolder string `json:"remoteWorkspaceFolder"`
 }
 
-type DevcontainerBuilderConfig struct {
-	buildId                      string
-	project                      workspace.Project
-	loggerFactory                logger.LoggerFactory
-	localContainerRegistryServer string
-	projectVolumePath            string
-}
-
 type DevcontainerBuilder struct {
-	BuilderPlugin
-	DevcontainerBuilderConfig
+	*Builder
 	buildImageName     string
 	user               string
 	postCreateCommands []string
@@ -88,7 +77,7 @@ func (b *DevcontainerBuilder) CleanUp() error {
 		return err
 	}
 
-	err = cli.ContainerRemove(ctx, b.buildId, container.RemoveOptions{
+	err = cli.ContainerRemove(ctx, b.id, container.RemoveOptions{
 		Force: true,
 	})
 	if err != nil {
@@ -184,7 +173,7 @@ func (b *DevcontainerBuilder) buildDevcontainer() error {
 		}
 	}(&buildOutcome)
 
-	result, err := dockerClient.ExecSync(b.buildId, execConfig, w)
+	result, err := dockerClient.ExecSync(b.id, execConfig, w)
 	if err != nil {
 		return err
 	}
@@ -207,7 +196,7 @@ func (b *DevcontainerBuilder) buildDevcontainer() error {
 
 	//	todo: commit sha from project Repository
 	tag := "latest"
-	imageName := fmt.Sprintf("%s:%s", b.localContainerRegistryServer+"/p-"+b.buildId, tag)
+	imageName := fmt.Sprintf("%s:%s", b.localContainerRegistryServer+"/p-"+b.id, tag)
 
 	_, err = builderCli.ContainerCommit(context.Background(), buildOutcome.ContainerId, container.CommitOptions{
 		Reference: imageName,
@@ -246,7 +235,7 @@ func (b *DevcontainerBuilder) readConfiguration() error {
 		Tty:          true,
 	}
 
-	result, err := dockerClient.ExecSync(b.buildId, execConfig, nil)
+	result, err := dockerClient.ExecSync(b.id, execConfig, nil)
 	if err != nil {
 		return err
 	}
@@ -305,7 +294,7 @@ func (b *DevcontainerBuilder) startContainer() error {
 		return err
 	}
 
-	dir := "/tmp/" + b.buildId + "/var/lib/docker"
+	dir := "/tmp/" + b.id + "/var/lib/docker"
 	err = os.MkdirAll(dir, 0755)
 	if err != nil {
 		return err
@@ -323,12 +312,12 @@ func (b *DevcontainerBuilder) startContainer() error {
 			filepath.Dir(b.getLocalDockerSocket()) + ":/tmp/docker",
 			dir + ":/var/lib/docker",
 		},
-	}, nil, nil, b.buildId)
+	}, nil, nil, b.id)
 	if err != nil {
 		return err
 	}
 
-	if err := cli.ContainerStart(ctx, b.buildId, container.StartOptions{}); err != nil {
+	if err := cli.ContainerStart(ctx, b.id, container.StartOptions{}); err != nil {
 		return err
 	}
 
@@ -349,7 +338,7 @@ func (b *DevcontainerBuilder) startDocker() error {
 		Cmd:          []string{"dockerd", "-H", "unix:///tmp/docker/docker.sock", "-H", "unix:///var/run/docker.sock", "--insecure-registry", b.localContainerRegistryServer},
 	}
 
-	execResp, err := cli.ContainerExecCreate(ctx, b.buildId, execConfig)
+	execResp, err := cli.ContainerExecCreate(ctx, b.id, execConfig)
 	if err != nil {
 		return err
 	}
@@ -380,5 +369,5 @@ func (b *DevcontainerBuilder) getBuilderDockerClient() (*client.Client, error) {
 }
 
 func (b *DevcontainerBuilder) getLocalDockerSocket() string {
-	return filepath.Join("/tmp", b.buildId, "docker", "docker.sock")
+	return filepath.Join("/tmp", b.id, "docker", "docker.sock")
 }
