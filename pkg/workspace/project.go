b3f594e228654e4ebf5c3855e28201969d756fa4
Vedran <vedran.jukic@gmail.com>
2024-05-22 16:57:33 +0000

    chore: comment our Dockerfile schema from the build config
    Signed-off-by: Vedran <vedran.jukic@gmail.com>

--- pkg/workspace/project.go
+++ pkg/workspace/project.go
@@ -18,15 +18,19 @@ type ProjectBuildDevcontainer struct {
 	DevContainerFilePath string `json:"devContainerFilePath"`
 } // @name ProjectBuildDevcontainer
 
+/*
 type ProjectBuildDockerfile struct {
 	Context    string            `json:"context"`
 	Dockerfile string            `json:"dockerfile"`
 	Args       map[string]string `json:"args"`
 } // @name ProjectBuildDockerfile
+*/
 
 type ProjectBuild struct {
 	Devcontainer *ProjectBuildDevcontainer `json:"devcontainer"`
-	Dockerfile   *ProjectBuildDockerfile   `json:"dockerfile"`
+	/*
+		Dockerfile   *ProjectBuildDockerfile   `json:"dockerfile"`
+	*/
 } // @name ProjectBuild
 
 type Project struct {
