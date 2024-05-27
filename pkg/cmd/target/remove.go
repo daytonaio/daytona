8f858e0af8f726ade44da7c277dcf6760cf72e45
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2024-05-27 16:01:33 +0300

    feat: ask user to remove workspaces when removing target (#599)
    Signed-off-by: mobley-trent <oyieko99@gmail.com>

--- pkg/cmd/target/remove.go
+++ pkg/cmd/target/remove.go
@@ -7,14 +7,18 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/charmbracelet/huh"
 	"github.com/daytonaio/daytona/cmd/daytona/config"
-	"github.com/daytonaio/daytona/internal/util/apiclient"
+	apiclient_util "github.com/daytonaio/daytona/internal/util/apiclient"
+	"github.com/daytonaio/daytona/pkg/apiclient"
 	"github.com/daytonaio/daytona/pkg/views"
 	"github.com/daytonaio/daytona/pkg/views/target"
 	"github.com/spf13/cobra"
 
 	log "github.com/sirupsen/logrus"
 )
+
+var yesFlag bool
 
 var targetRemoveCmd = &cobra.Command{
 	Use:     "remove [TARGET_NAME]",
@@ -35,7 +39,7 @@ var targetRemoveCmd = &cobra.Command{
 				log.Fatal(err)
 			}
 
-			targets, err := apiclient.GetTargetList()
+			targets, err := apiclient_util.GetTargetList()
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -50,16 +54,76 @@ var targetRemoveCmd = &cobra.Command{
 			selectedTargetName = args[0]
 		}
 
-		client, err := apiclient.GetApiClient(nil)
+		ctx := context.Background()
+		client, err := apiclient_util.GetApiClient(nil)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		res, err := client.TargetAPI.RemoveTarget(context.Background(), selectedTargetName).Execute()
+		if yesFlag {
+			fmt.Println("Deleting all workspaces.")
+			err := RemoveTargetWorkspaces(ctx, client, selectedTargetName)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			form := huh.NewForm(
+				huh.NewGroup(
+					huh.NewConfirm().
+						Title(fmt.Sprintf("Delete all workspaces within %s?", selectedTargetName)).
+						Description("You might not be able to easily remove these workspaces later.").
+						Value(&yesFlag),
+				),
+			).WithTheme(views.GetCustomTheme())
+
+			err := form.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if yesFlag {
+				err := RemoveTargetWorkspaces(ctx, client, selectedTargetName)
+				if err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				fmt.Println("Proceeding with target removal without deleting workspaces.")
+			}
+		}
+
+		res, err := client.TargetAPI.RemoveTarget(ctx, selectedTargetName).Execute()
 		if err != nil {
-			log.Fatal(apiclient.HandleErrorResponse(res, err))
+			log.Fatal(apiclient_util.HandleErrorResponse(res, err))
 		}
 
 		views.RenderInfoMessageBold(fmt.Sprintf("Target %s removed successfully", selectedTargetName))
 	},
+}
+
+func init() {
+	targetRemoveCmd.Flags().BoolVarP(&yesFlag, "yes", "y", false, "Confirm deletion of all workspaces without prompt")
+}
+
+func RemoveTargetWorkspaces(ctx context.Context, client *apiclient.APIClient, target string) error {
+	workspaceList, res, err := client.WorkspaceAPI.ListWorkspaces(ctx).Execute()
+	if err != nil {
+		return apiclient_util.HandleErrorResponse(res, err)
+	}
+
+	for _, workspace := range workspaceList {
+		if *workspace.Target != target {
+			continue
+		}
+
+		res, err := client.WorkspaceAPI.RemoveWorkspace(ctx, *workspace.Id).Execute()
+		if err != nil {
+			log.Errorf("Failed to delete workspace %s: %v", *workspace.Name, apiclient_util.HandleErrorResponse(res, err))
+			continue
+		}
+
+		views.RenderLine(fmt.Sprintf("- Workspace %s successfully deleted\n", *workspace.Name))
+	}
+
+	return nil
 }
