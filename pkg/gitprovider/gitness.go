c841a06788cc3b3f77a3abaf51afb6480584d55c
rutik7066 <rutikthakre@gmail.com>
2024-06-16 19:22:55 +0530

    sign off

--- pkg/gitprovider/gitness.go
+++ pkg/gitprovider/gitness.go
@@ -171,6 +171,7 @@ func (g *GitNessGitProvider) getPrContext(staticContext *StaticGitContext) (*Sta
 }
 
 func (g *GitNessGitProvider) parseStaticGitContext(repoUrl string) (*StaticGitContext, error) {
+	fmt.Println("parseStaticGitContext.repoUrl: ", repoUrl)
 	staticContext, err := g.AbstractGitProvider.parseStaticGitContext(repoUrl)
 	if err != nil {
 		return nil, err
