d867ab6cf9b963bd0aa0001fba73f221efdda253
Abhishek Kumar Gupta <abhishekguptaatweb17@gmail.com>
2024-07-22 19:44:27 +0000

    fix tests and run lint (#761)
    Signed-off-by: Abhishek Kumar Gupta <abhishekguptaatweb17@gmail.com>

--- internal/testing/server/workspaces/mocks/git_provider_service.go
+++ internal/testing/server/workspaces/mocks/git_provider_service.go
@@ -50,22 +50,22 @@ func (m *MockGitProviderService) GetGitUser(gitProviderId string) (*gitprovider.
 	return args.Get(0).(*gitprovider.GitUser), args.Error(1)
 }
 
-func (m *MockGitProviderService) GetNamespaces(gitProviderId string) ([]*gitprovider.GitNamespace, error) {
+func (m *MockGitProviderService) GetNamespaces(gitProviderId string, options gitprovider.ListOptions) ([]*gitprovider.GitNamespace, error) {
 	args := m.Called(gitProviderId)
 	return args.Get(0).([]*gitprovider.GitNamespace), args.Error(1)
 }
 
-func (m *MockGitProviderService) GetRepoBranches(gitProviderId string, namespaceId string, repositoryId string) ([]*gitprovider.GitBranch, error) {
+func (m *MockGitProviderService) GetRepoBranches(gitProviderId string, namespaceId string, repositoryId string, options gitprovider.ListOptions) ([]*gitprovider.GitBranch, error) {
 	args := m.Called(gitProviderId, namespaceId, repositoryId)
 	return args.Get(0).([]*gitprovider.GitBranch), args.Error(1)
 }
 
-func (m *MockGitProviderService) GetRepoPRs(gitProviderId string, namespaceId string, repositoryId string) ([]*gitprovider.GitPullRequest, error) {
+func (m *MockGitProviderService) GetRepoPRs(gitProviderId string, namespaceId string, repositoryId string, options gitprovider.ListOptions) ([]*gitprovider.GitPullRequest, error) {
 	args := m.Called(gitProviderId, namespaceId, repositoryId)
 	return args.Get(0).([]*gitprovider.GitPullRequest), args.Error(1)
 }
 
-func (m *MockGitProviderService) GetRepositories(gitProviderId string, namespaceId string, page, perPage int) ([]*gitprovider.GitRepository, error) {
+func (m *MockGitProviderService) GetRepositories(gitProviderId string, namespaceId string, options gitprovider.ListOptions) ([]*gitprovider.GitRepository, error) {
 	args := m.Called(gitProviderId, namespaceId)
 	return args.Get(0).([]*gitprovider.GitRepository), args.Error(1)
 }
