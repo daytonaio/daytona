c3dc3b4d7442ca484bb360f253f2c0d18b0bba3d
Ivan Dagelić <dagelic.ivan@gmail.com>
2024-08-02 15:40:10 +0200

    chore: swagger version update to 0.24.0 (#889)
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- pkg/api/docs/docs.go
+++ pkg/api/docs/docs.go
@@ -2004,7 +2004,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.1.0",
+	Version:          "0.24.0",
 	Host:             "localhost:3986",
 	BasePath:         "/",
 	Schemes:          []string{"http"},
