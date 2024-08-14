7a0d41f5b963b7985e23ea314f4c06d85187a7f4
johnwick <jiosupprt270@gmail.com>
2024-08-14 07:00:37 +0000

    azure
    Signed-off-by: johnwick <jiosupprt270@gmail.com>

--- pkg/gitprovider/azure-devops.go
+++ pkg/gitprovider/azure-devops.go
@@ -254,6 +254,71 @@ func (g *AzureDevOpsGitProvider) GetLastCommitSha(staticContext *StaticGitContex
 	return *(*commits)[0].CommitId, nil
 }
 
+func (g *AzureDevOpsGitProvider) GetBranchByCommit(staticContext *StaticGitContext) (string, error) {
+	if staticContext.Sha == nil || *staticContext.Sha == "" {
+		return *staticContext.Branch, nil
+	}
+
+	client, err := g.getGitClient()
+	if err != nil {
+		return "", err
+	}
+
+	branches, err := client.GetBranches(context.Background(), git.GetBranchesArgs{
+		RepositoryId: &staticContext.Id,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	var branchName string
+	for _, branch := range *branches {
+		if *branch.Commit.CommitId == *staticContext.Sha {
+			branchName = *branch.Name
+			break
+		}
+
+		searchCriteria := &git.GitQueryCommitsCriteria{
+			ItemVersion: &git.GitVersionDescriptor{
+				Version:     &branchName,
+				VersionType: &git.GitVersionTypeValues.Branch,
+			},
+			FromCommitId: staticContext.Sha,
+			ToCommitId:   staticContext.Sha,
+		}
+
+		commits, err := client.GetCommitsBatch(context.Background(), git.GetCommitsBatchArgs{
+			SearchCriteria: searchCriteria,
+			RepositoryId:   &staticContext.Name,
+		})
+		if err != nil {
+			return "", err
+		}
+
+		if len(*commits) == 0 {
+			continue
+		}
+
+		for _, commit := range *commits {
+			if *commit.CommitId == *staticContext.Sha {
+				branchName = *branch.Name
+				break
+			}
+
+		}
+		if branchName != "" {
+			break
+		}
+
+	}
+
+	if branchName == "" {
+		return "", fmt.Errorf("branch not found for SHA: %s", *staticContext.Sha)
+	}
+
+	return branchName, nil
+}
+
 func (g *AzureDevOpsGitProvider) GetUrlFromRepository(repo *GitRepository) string {
 	url := strings.TrimSuffix(repo.Url, ".git")
 	url = strings.TrimSuffix(url, repo.Name)
