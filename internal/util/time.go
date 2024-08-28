3085933b48de6be1029abf09d9766637c18dbc82
Ivan Dagelić <dagelic.ivan@gmail.com>
2024-08-28 15:02:24 +0200

    fix: timestamp with timezone (#981)
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- internal/util/time.go
+++ internal/util/time.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-var timeLayout = "2006-01-02T15:04:05.999999999Z"
+var timeLayout = "2006-01-02T15:04:05.999999999Z07:00"
 
 func FormatTimestamp(input string) string {
 	t, err := time.Parse(timeLayout, input)
