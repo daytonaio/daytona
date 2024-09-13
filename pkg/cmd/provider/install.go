665cb852db242293729bc509ae4161fe1543cada
Toma Puljak <toma.puljak@hotmail.com>
2024-09-13 10:51:59 +0200

    feat: suggest adding a target after provider install (#1095)
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- pkg/cmd/provider/install.go
+++ pkg/cmd/provider/install.go
@@ -4,8 +4,11 @@
 package provider
 
 import (
+	"context"
 	"fmt"
+	"slices"
 
+	"github.com/charmbracelet/huh"
 	apiclient_util "github.com/daytonaio/daytona/internal/util/apiclient"
 	"github.com/daytonaio/daytona/pkg/apiclient"
 	"github.com/daytonaio/daytona/pkg/common"
@@ -14,11 +17,14 @@ import (
 	"github.com/daytonaio/daytona/pkg/views"
 	"github.com/daytonaio/daytona/pkg/views/provider"
 	provider_view "github.com/daytonaio/daytona/pkg/views/provider"
+	"github.com/daytonaio/daytona/pkg/views/target"
 	views_util "github.com/daytonaio/daytona/pkg/views/util"
 	"github.com/spf13/cobra"
 
 	log "github.com/sirupsen/logrus"
 )
+
+var yesFlag bool
 
 var providerInstallCmd = &cobra.Command{
 	Use:     "install",
@@ -93,7 +99,72 @@ var providerInstallCmd = &cobra.Command{
 		}
 
 		views.RenderInfoMessageBold(fmt.Sprintf("Provider %s has been successfully installed", providerToInstall.Name))
+
+		targets, res, err := apiClient.TargetAPI.ListTargets(context.Background()).Execute()
+		if err != nil {
+			log.Fatal(apiclient_util.HandleErrorResponse(res, err))
+		}
+
+		if slices.ContainsFunc(targets, func(t apiclient.ProviderTarget) bool {
+			return t.ProviderInfo.Name == providerToInstall.Name
+		}) {
+			return
+		}
+
+		if !yesFlag {
+			form := huh.NewForm(
+				huh.NewGroup(
+					huh.NewConfirm().
+						Title("Add a Target?").
+						Value(&yesFlag),
+				),
+			).WithTheme(views.GetCustomTheme())
+
+			err := form.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if yesFlag {
+			targetManifest, res, err := apiClient.ProviderAPI.GetTargetManifest(context.Background(), providerToInstall.Name).Execute()
+			if err != nil {
+				log.Fatal(apiclient_util.HandleErrorResponse(res, err))
+			}
+
+			targetToSet := &apiclient.ProviderTarget{
+				Options: "{}",
+				ProviderInfo: apiclient.ProviderProviderInfo{
+					Name:    providerToInstall.Name,
+					Version: providerToInstall.Version,
+				},
+			}
+
+			err = target.NewTargetNameInput(&targetToSet.Name, []string{})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = target.SetTargetForm(targetToSet, *targetManifest)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			res, err = apiClient.TargetAPI.SetTarget(context.Background()).Target(*targetToSet).Execute()
+			if err != nil {
+				log.Fatal(apiclient_util.HandleErrorResponse(res, err))
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			views.RenderInfoMessage("Target set successfully")
+		}
 	},
+}
+
+func init() {
+	providerInstallCmd.Flags().BoolVarP(&yesFlag, "yes", "y", false, "Automatically confirm any prompts")
 }
 
 func GetProviderListFromManifest(manifest *manager.ProvidersManifest) []apiclient.Provider {
