e11697ed3e37c997a7301a0f80b9f6860fcb5a56
Toma Puljak <toma.puljak@hotmail.com>
2024-09-23 11:36:22 +0000

    feat: auto-copy api key to clipboard
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- pkg/views/server/apikey/notify.go
+++ pkg/views/server/apikey/notify.go
@@ -5,41 +5,29 @@ package apikey
 
 import (
 	"fmt"
-	"os"
 
+	"github.com/atotto/clipboard"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/daytonaio/daytona/pkg/views"
-	"golang.org/x/term"
 )
-
-var minimumLayoutWidth = 80
 
 func Render(key, apiUrl string) {
 	var output string
 
-	terminalWidth, _, err := term.GetSize(int(os.Stdout.Fd()))
-	if err != nil {
-		fmt.Println("error: Unable to get terminal size")
-		return
-	}
-
 	output += fmt.Sprintf("%s %s", views.GetPropertyKey("Generated API key: "), key) + "\n\n"
-
-	output += "Make sure to copy it as you will not be able to see it again." + "\n\n"
-
-	output += views.SeparatorString + "\n\n"
 
 	output += "You can connect to the Daytona Server from a client machine by running:"
 
-	formattedCommand := lipgloss.NewStyle().Foreground(views.Green).Render(fmt.Sprintf("daytona profile add -a \\\n%s \\\n-k %s", apiUrl, key))
-	command := lipgloss.NewStyle().Foreground(views.Green).Render(fmt.Sprintf("daytona profile add -a %s -k %s", apiUrl, key))
+	views.RenderContainerLayout(views.GetInfoMessage(output))
 
-	if terminalWidth >= minimumLayoutWidth {
-		output += "\n\n" + formattedCommand
-		views.RenderContainerLayout(views.GetInfoMessage(output))
+	command := fmt.Sprintf("daytona profile add -a %s -k %s", apiUrl, key)
+	fmt.Println(lipgloss.NewStyle().Padding(0).Foreground(views.Green).Render(command) + "\n\n")
+
+	if err := clipboard.WriteAll(command); err == nil {
+		output = "The command has been copied to your clipboard."
 	} else {
-		views.RenderContainerLayout(views.GetInfoMessage(output))
-		fmt.Println(command + "\n\n")
+		output = "Make sure to copy it as you will not be able to see it again."
 	}
 
+	views.RenderContainerLayout(views.GetInfoMessage(output))
 }
