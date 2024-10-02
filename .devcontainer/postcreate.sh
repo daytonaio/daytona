34e88cb43f518c1d9cac851230d803fe198ae627
Ivan Dagelic <dagelic.ivan@gmail.com>
2024-10-02 10:19:12 +0200

    chore: add newline
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for rc in ~/.bashrc ~/.zshrc; do [ -f "$rc" ] && echo 'alias dtn="DAYTONA_DEV=1 DAYTONA_CONFIG_DIR=$HOME/.config/daytona-dev go run cmd/daytona/main.go"' >> $rc; done
+for rc in ~/.bashrc ~/.zshrc; do [ -f "$rc" ] && echo -e '\nalias dtn="DAYTONA_DEV=1 DAYTONA_CONFIG_DIR=$HOME/.config/daytona-dev go run cmd/daytona/main.go"' >> $rc; done
 
 go install github.com/swaggo/swag/cmd/swag@v1.16.3
 
