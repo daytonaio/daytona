bbd177477832a98b76f4b8c2a89699828fbb81b3
bryans-go <sabrinabryan1990@gmail.com>
2024-10-09 20:09:20 +0530

    add case for ctrl+c
    Signed-off-by: bryans-go <sabrinabryan1990@gmail.com>

--- pkg/views/util/spinner.go
+++ pkg/views/util/spinner.go
@@ -40,11 +40,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.quitting = true
 		return m, tea.Quit
 
-	default:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+		}
 	}
+	var cmd tea.Cmd
+	m.spinner, cmd = m.spinner.Update(msg)
+	return m, cmd
+
 }
 
 func WithSpinner(message string, fn func() error) error {
