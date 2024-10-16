e3d7be198b93d3b9ec4a7ea9b97bcf643c59f682
Abinand P <abinand0911@gmail.com>
2024-10-16 08:04:10 +0000

    fix: created a new file text_wrapper to have the functions to wrap the text withtin the terminal
    Signed-off-by: Abinand P <abinand0911@gmail.com>

---
+++ pkg/views/util/text_wrapper.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"os"
+	"strings"
+	"unicode/utf8"
+
+	"golang.org/x/term"
+)
+
+func WrapText(text string, width int) string {
+	if width <= 0 {
+		return text
+	}
+
+	var wrappedText strings.Builder
+	var line strings.Builder
+
+	words := strings.Fields(text)
+
+	for _, word := range words {
+		if utf8.RuneCountInString(line.String())+utf8.RuneCountInString(word)+1 > width-2 {
+			wrappedText.WriteString(line.String() + "\n")
+			line.Reset()
+		}
+
+		if line.Len() > 0 {
+			line.WriteString(" ")
+		}
+		line.WriteString(word)
+	}
+
+	if line.Len() > 0 {
+		wrappedText.WriteString(line.String())
+	}
+
+	return wrappedText.String()
+}
+
+func GetTerminalWidth() int {
+	width, _, err := term.GetSize(int(os.Stdout.Fd()))
+	const maxWidth = 160
+	if err != nil {
+		return maxWidth
+	}
+	return width
+}
