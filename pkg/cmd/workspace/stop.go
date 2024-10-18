573167867c5619be7824aa44c67b09f603422dd4
Luka Brecic <luka.brecic3@gmail.com>
2024-10-18 12:27:46 +0000

    fix: autocompletion
    Signed-off-by: Luka Brecic <luka.brecic3@gmail.com>

--- pkg/cmd/workspace/stop.go
+++ pkg/cmd/workspace/stop.go
@@ -111,10 +111,6 @@ var StopCmd = &cobra.Command{
 		return nil
 	},
 	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-		if len(args) >= 1 {
-			return nil, cobra.ShellCompDirectiveNoFileComp
-		}
-
 		return getAllWorkspacesByState(WORKSPACE_STATUS_RUNNING)
 	},
 }
