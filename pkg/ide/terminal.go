2eba5de0bc90c860621a9981bb7fb5c8a0fa7653
bryans-go <sabrinabryan1990@gmail.com>
2024-10-21 21:50:44 +0530

    refactor code
    Signed-off-by: bryans-go <sabrinabryan1990@gmail.com>

--- pkg/ide/terminal.go
+++ pkg/ide/terminal.go
@@ -13,32 +13,19 @@ import (
 )
 
 func OpenTerminalSsh(activeProfile config.Profile, workspaceId string, projectName string, gpgKey string, sshOptions []string, args ...string) error {
-	err := config.EnsureSshConfigEntryAdded(activeProfile.Id, workspaceId, projectName, gpgKey)
+
+	if err := config.EnsureSshConfigEntryAdded(activeProfile.Id, workspaceId, projectName, gpgKey); err != nil {
+		return err
+	}
+
+	// Parse SSH options
+	parsedOptions, err := parseSshOptions(sshOptions)
 	if err != nil {
 		return err
 	}
 
-	parsedOptions := make(map[string]string)
-	for _, option := range sshOptions {
-		parts := strings.SplitN(option, "=", 2)
-		if len(parts) == 1 {
-			return fmt.Errorf("no argument after keyword %q", parts[0])
-		}
-		if len(parts) != 2 || strings.Count(option, "=") > 1 {
-			return fmt.Errorf("bad configuration option: %s", option)
-		}
-		parsedOptions[strings.TrimSpace(parts[0])] = strings.TrimSpace(parts[1])
-	}
-
 	projectHostname := config.GetProjectHostname(activeProfile.Id, workspaceId, projectName)
-
-	cmdArgs := []string{projectHostname}
-
-	for key, value := range parsedOptions {
-		cmdArgs = append(cmdArgs, "-o", fmt.Sprintf("%s=%s", key, value))
-	}
-
-	cmdArgs = append(cmdArgs, args...)
+	cmdArgs := buildCommandArgs(projectHostname, parsedOptions, args...)
 
 	sshCommand := exec.Command("ssh", cmdArgs...)
 	sshCommand.Stdin = os.Stdin
@@ -46,4 +33,28 @@ func OpenTerminalSsh(activeProfile config.Profile, workspaceId string, projectNa
 	sshCommand.Stderr = os.Stderr
 
 	return sshCommand.Run()
+}
+
+// parseSshOptions validates and parses the SSH options.
+func parseSshOptions(sshOptions []string) (map[string]string, error) {
+	parsedOptions := make(map[string]string)
+	for _, option := range sshOptions {
+		parts := strings.SplitN(option, "=", 2)
+		if len(parts) == 1 {
+			return nil, fmt.Errorf("no argument after keyword %q", parts[0])
+		}
+		if len(parts) != 2 || strings.Count(option, "=") > 1 {
+			return nil, fmt.Errorf("bad configuration option: %s", option)
+		}
+		parsedOptions[strings.TrimSpace(parts[0])] = strings.TrimSpace(parts[1])
+	}
+	return parsedOptions, nil
+}
+
+func buildCommandArgs(projectHostname string, parsedOptions map[string]string, args ...string) []string {
+	cmdArgs := []string{projectHostname}
+	for key, value := range parsedOptions {
+		cmdArgs = append(cmdArgs, "-o", fmt.Sprintf("%s=%s", key, value))
+	}
+	return append(cmdArgs, args...)
 }
