8a213a8e123e2863fe46a029e172c5f2d4030035
Toma Puljak <toma.puljak@hotmail.com>
2024-10-25 12:33:43 +0200

    fix: connection troubleshooting help (#1290)
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- pkg/common/error.go
+++ pkg/common/error.go
@@ -5,6 +5,7 @@ package common
 
 import (
 	"errors"
+	"strings"
 )
 
 var (
@@ -13,4 +14,12 @@ var (
 
 func IsCtrlCAbort(err error) bool {
 	return err.Error() == ErrCtrlCAbort.Error()
+}
+
+var (
+	ErrConnection = errors.New("If you are using a VPN or firewall, please read our troubleshooting guide at https://daytona.io/docs/misc/troubleshooting#connectivity-issues")
+)
+
+func IsConnectionError(err error) bool {
+	return strings.Contains(err.Error(), ErrConnection.Error())
 }
