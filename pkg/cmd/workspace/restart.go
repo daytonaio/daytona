e1bb17617df2526cf90a13bb2efe30419263e4a2
Luka Brečić <luka.brecic3@gmail.com>
2024-12-05 00:15:49 +0100

    feat: disable items for selection when in invalid state (#1412)
    Signed-off-by: Luka Brecic <luka.brecic3@gmail.com>

--- pkg/cmd/workspace/restart.go
+++ pkg/cmd/workspace/restart.go
@@ -44,7 +44,7 @@ var RestartCmd = &cobra.Command{
 		}
 
 		if len(args) == 0 {
-			selectedWorkspaces = selection.GetWorkspacesFromPrompt(workspaceList, "Restart")
+			selectedWorkspaces = selection.GetWorkspacesFromPrompt(workspaceList, selection.RestartActionVerb)
 			if selectedWorkspaces == nil {
 				return nil
 			}
