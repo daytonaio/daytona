a19298196d463bf6d0552cace5f859d894080d93
Luka Brečić <luka.brecic3@gmail.com>
2024-12-05 16:00:16 +0100

    refactor: enable container registry setting through env-vars (#1406)
    Signed-off-by: Luka Brecic <luka.brecic3@gmail.com>

--- pkg/server/targets/set-default.go
+++ pkg/server/targets/set-default.go
@@ -7,7 +7,6 @@ import (
 	"context"
 
 	"github.com/daytonaio/daytona/internal/util"
-	"github.com/daytonaio/daytona/pkg/models"
 	"github.com/daytonaio/daytona/pkg/services"
 	"github.com/daytonaio/daytona/pkg/stores"
 )
@@ -29,22 +28,12 @@ func (s *TargetService) SetDefault(ctx context.Context, id string) error {
 
 	if defaultTarget != nil {
 		defaultTarget.IsDefault = false
-		err := s.targetStore.Save(TargetDtoToTarget(*defaultTarget))
+		err := s.targetStore.Save(&defaultTarget.Target)
 		if err != nil {
 			return err
 		}
 	}
 
 	currentTarget.IsDefault = true
-	return s.targetStore.Save(TargetDtoToTarget(*currentTarget))
-}
-
-func TargetDtoToTarget(targetDto services.TargetDTO) *models.Target {
-	return &models.Target{
-		Id:             targetDto.Id,
-		Name:           targetDto.Name,
-		TargetConfigId: targetDto.TargetConfigId,
-		TargetConfig:   targetDto.TargetConfig,
-		IsDefault:      targetDto.IsDefault,
-	}
+	return s.targetStore.Save(&currentTarget.Target)
 }
