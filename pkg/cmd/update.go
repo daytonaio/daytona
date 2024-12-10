c1fbc73c1f900815d75b8369c39e8d711f8b72d1
Abinand P <abinand0911@gmail.com>
2024-12-10 14:15:11 +0000

    chore: moved the utitly funtion and used the semver to compare the semantic versions and other fix from requested changes
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- pkg/cmd/update.go
+++ pkg/cmd/update.go
@@ -5,13 +5,16 @@ package cmd
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/daytonaio/daytona/internal"
 	"github.com/inconshreveable/go-update"
 	"github.com/spf13/cobra"
+	"golang.org/x/mod/semver"
 	"net/http"
+	"net/url"
+	"regexp"
 	"runtime"
-	"strconv"
 	"strings"
 )
 
@@ -23,6 +26,65 @@ const (
 type GitHubRelease struct {
 	TagName   string `json:"tag_name"`
 	ChangeLog string `json:"body"`
+}
+
+var versionFlag string
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Update Daytona CLI",
+	Long:  "Update Daytona CLI to the latest version",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var version string
+		var changeLog string
+		currentVersion := internal.Version
+		if versionFlag == "" {
+			release, err := fetchLatestRelase()
+			if err != nil {
+				return err
+			}
+			version = release.TagName
+			changeLog = release.ChangeLog
+		} else {
+			semverRegex := `^v?(\d+\.\d+\.\d+)$`
+			matched, err := regexp.MatchString(semverRegex, versionFlag)
+			if err != nil {
+				return err
+			}
+			if !matched {
+				return errors.New("invalid version format: expected 'vX.Y.X' or 'X.Y.Z'")
+			}
+			if !strings.HasPrefix(versionFlag, "v") {
+				versionFlag = "v" + versionFlag
+			}
+			release, err := fetchVersionRelease(versionFlag)
+			if err != nil {
+				return err
+			}
+			version = release.TagName
+			changeLog = release.ChangeLog
+		}
+		if semver.Compare(version, currentVersion) <= 0 {
+			fmt.Println("Current version is greater than the version you are trying to update to")
+			return nil
+		}
+		if semver.Compare(currentVersion, version) != 0 {
+			changeLog += "\nThere are more changes in the version. Please visit https://github.com/daytonaio/daytona/releases for all the complete chnageLog"
+		}
+		fmt.Println("Updating to version ", version, "from ", currentVersion)
+		fmt.Println("ChangeLog:")
+		fmt.Println(changeLog)
+		err := updateToVersion(version)
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+}
+
+func init() {
+	updateCmd.Flags().StringVarP(&versionFlag, "version", "v", "", "Version to update to")
+
 }
 
 func fetchLatestRelase() (*GitHubRelease, error) {
@@ -66,7 +128,10 @@ func fetchVersionRelease(version string) (*GitHubRelease, error) {
 }
 
 func updateToVersion(version string) error {
-	url := getBinaryUrl(version)
+	url, err := getBinaryUrl(version)
+	if err != nil {
+		return fmt.Errorf("failed to get binary url: %w", err)
+	}
 	resp, err := http.Get(url)
 	if err != nil {
 		return fmt.Errorf("failed to fetch binary: %w", err)
@@ -87,115 +152,14 @@ func updateToVersion(version string) error {
 	return nil
 }
 
-func getBinaryUrl(version string) string {
+func getBinaryUrl(version string) (string, error) {
+	fileName := fmt.Sprintf("daytona-%s-%s", runtime.GOOS, runtime.GOARCH)
 	if runtime.GOOS == "windows" {
-		return baseDownloadURL + fmt.Sprintf("%s/daytona-%s-%s.exe", version, runtime.GOOS, runtime.GOARCH)
+		fileName += ".exe"
 	}
-	return baseDownloadURL + fmt.Sprintf("%s/daytona-%s-%s", version, runtime.GOOS, runtime.GOARCH)
-}
-func isVersionGreater(version string, currentVersion string) bool {
-	version = strings.TrimPrefix(version, "v")
-	currentVersion = strings.TrimPrefix(currentVersion, "v")
-	v1 := strings.Split(version, ".")
-	v2 := strings.Split(currentVersion, ".")
-	maxLength := len(v1)
-	if len(v2) > maxLength {
-		maxLength = len(v2)
-	}
-	for i := 0; i < maxLength; i++ {
-		var ver1 int
-		var ver2 int
-		if i < len(v1) {
-			ver1, _ = strconv.Atoi(v1[i])
-		}
-		if i < len(v2) {
-			ver2, _ = strconv.Atoi(v2[i])
-		}
-		if ver1 > ver2 {
-			return false
-		}
-		if ver1 < ver2 {
-			return true
-		}
-	}
-	return false
-}
-func isCurrentVersionEqualToPrevious(currentVersion string, version string) (bool, error) {
-	resp, err := http.Get(githubReleaseURL)
+	fullURL, err := url.JoinPath(baseDownloadURL, version, fileName)
 	if err != nil {
-		return false, fmt.Errorf("failed to fetch latest release: %w", err)
+		return "", err
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return false, fmt.Errorf("failed to fetch latest release: %s", resp.Status)
-	}
-	var releases []GitHubRelease
-	if err := json.NewDecoder(resp.Body).Decode(&releases); err != nil {
-		return false, fmt.Errorf("failed to fetch latest release: %w", err)
-	}
-	if len(releases) == 0 {
-		return false, fmt.Errorf("no releases found")
-	}
-	for i := 0; i < len(releases); i++ {
-		if releases[i].TagName == version {
-			if i+1 < len(releases) {
-				if releases[i+1].TagName == currentVersion {
-					return true, nil
-				}
-			}
-		}
-	}
-	return false, nil
-}
-
-var versionFlag string
-var updateCmd = &cobra.Command{
-	Use:   "update",
-	Short: "Update Daytona CLI",
-	Long:  "Update Daytona CLI to the latest version",
-	Args:  cobra.NoArgs,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		var version string
-		var changeLog string
-		currentVersion := internal.Version
-		if versionFlag == "" {
-			release, err := fetchLatestRelase()
-			if err != nil {
-				return err
-			}
-			version = release.TagName
-			changeLog = release.ChangeLog
-		} else {
-			release, err := fetchVersionRelease("v" + versionFlag)
-			if err != nil {
-				return err
-			}
-			version = release.TagName
-			changeLog = release.ChangeLog
-		}
-		if isVersionGreater(version, currentVersion) {
-			fmt.Println("Current version is greater than the version you are trying to update to")
-			return nil
-		}
-		isCurrEqualPrev, err := isCurrentVersionEqualToPrevious(currentVersion, version)
-		if err != nil {
-			return err
-		}
-		if !isCurrEqualPrev {
-			changeLog += "\nThere are more changes in the version. Please visit https://github.com/daytonaio/daytona/releases for all the complete chnageLog"
-		}
-		fmt.Println("Updating to version ", version, "from ", currentVersion)
-		fmt.Println("ChangeLog:")
-		fmt.Println(changeLog)
-		err = updateToVersion(version)
-		if err != nil {
-			return err
-		}
-		return nil
-	},
-}
-
-func init() {
-	updateCmd.Flags().StringVarP(&versionFlag, "version", "v", "", "Version to update to")
-	rootCmd.AddCommand(updateCmd)
+	return fullURL, nil
 }
