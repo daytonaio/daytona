8ddd31a918b6f9b869bcf94517f288111b777674
Abinand P <abinand0911@gmail.com>
2024-12-13 14:51:05 +0000

    formatted and made similar to the codebase
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- pkg/cmd/update.go
+++ pkg/cmd/update.go
@@ -29,6 +29,7 @@ type GitHubRelease struct {
 }
 
 var versionFlag string
+
 var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update Daytona CLI",
@@ -38,47 +39,58 @@ var updateCmd = &cobra.Command{
 		var version string
 		var changelog string
 		currentVersion := internal.Version
+
 		if versionFlag == "" {
 			release, err := fetchLatestRelase()
 			if err != nil {
 				return err
 			}
+
 			version = release.TagName
 			changelog = release.Changelog
+
 		} else {
 			semverRegex := `^v?(\d+\.\d+\.\d+)$`
 			matched, err := regexp.MatchString(semverRegex, versionFlag)
+
 			if err != nil {
 				return err
 			}
+
 			if !matched {
 				return errors.New("invalid version format: expected 'vX.Y.X' or 'X.Y.Z'")
 			}
+
 			if !strings.HasPrefix(versionFlag, "v") {
 				versionFlag = "v" + versionFlag
 			}
+
 			release, err := fetchVersionRelease(versionFlag)
 			if err != nil {
 				return err
 			}
+
 			version = release.TagName
 			changelog = release.Changelog
 		}
+
+		if semver.Compare(currentVersion, version) >= 0 {
+			return errors.New("current version is greater than the version you are trying to update to")
+		}
+
 		isCurrVerEqToPrev, err := isCurrentVersionEqualToPrevious(currentVersion, version)
 		if err != nil {
 			return err
 		}
 
-		if semver.Compare(currentVersion, version) >= 0 {
-			return errors.New("current version is greater than the version you are trying to update to")
-
-		}
 		if !isCurrVerEqToPrev {
 			changelog += "\n\nThere might be more important changes since you updated. Please visit https://github.com/daytonaio/daytona/releases for the complete changelog\n"
 		}
+
 		fmt.Println("Updating to version", version, "from", currentVersion)
 		fmt.Print("\nChangelog:\n\n")
 		fmt.Println(changelog)
+
 		return updateToVersion(version)
 	},
 }
@@ -89,13 +101,16 @@ func init() {
 
 func fetchLatestRelase() (*GitHubRelease, error) {
 	resp, err := http.Get(githubReleaseURL + "/latest")
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch latest release: %w", err)
 	}
+
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("failed to fetch latest release: %s", resp.Status)
 	}
+
 	var release GitHubRelease
 	if err := json.NewDecoder(resp.Body).Decode(&release); err != nil {
 		return nil, fmt.Errorf("failed to fetch latest release: %w", err)
@@ -108,17 +123,21 @@ func fetchVersionRelease(version string) (*GitHubRelease, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch latest release: %w", err)
 	}
+
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("failed to fetch latest release: %s", resp.Status)
 	}
+
 	var releases []GitHubRelease
+
 	if err := json.NewDecoder(resp.Body).Decode(&releases); err != nil {
 		return nil, fmt.Errorf("failed to fetch latest release: %w", err)
 	}
 	if len(releases) == 0 {
 		return nil, fmt.Errorf("no releases found")
 	}
+
 	for _, release := range releases {
 		if release.TagName == version {
 			return &release, nil
@@ -128,33 +147,43 @@ func fetchVersionRelease(version string) (*GitHubRelease, error) {
 }
 
 func updateToVersion(version string) error {
+
 	url, err := getBinaryUrl(version)
 	if err != nil {
 		return fmt.Errorf("failed to get binary url: %w", err)
 	}
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return fmt.Errorf("failed to fetch binary: %w", err)
 	}
+
 	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("failed to fetch binary: %s", resp.Status)
 	}
+
 	err = update.Apply(resp.Body, update.Options{})
+
 	if err != nil {
 		fmt.Println("Failed to update binary")
 		if rollbackErr := update.RollbackError(err); rollbackErr != nil {
 			return fmt.Errorf("failed to rollback: %w", rollbackErr)
 		}
+
 		return fmt.Errorf("failed to update binary: %w", err)
 	}
+
 	fmt.Println("\nSuccessfully updated to version", version)
 	fmt.Println("If your Server is running, you need to restart it for the changes to take effect.")
 	return nil
 }
 
 func getBinaryUrl(version string) (string, error) {
+
 	fileName := fmt.Sprintf("daytona-%s-%s", runtime.GOOS, runtime.GOARCH)
+
 	if runtime.GOOS == "windows" {
 		fileName += ".exe"
 	}
@@ -162,25 +191,32 @@ func getBinaryUrl(version string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+
 	return fullURL, nil
 }
 
 func isCurrentVersionEqualToPrevious(currentVersion string, version string) (bool, error) {
+
 	resp, err := http.Get(githubReleaseURL)
 	if err != nil {
 		return false, fmt.Errorf("failed to fetch latest release: %w", err)
 	}
+
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		return false, fmt.Errorf("failed to fetch latest release: %s", resp.Status)
 	}
+
 	var releases []GitHubRelease
+
 	if err := json.NewDecoder(resp.Body).Decode(&releases); err != nil {
 		return false, fmt.Errorf("failed to fetch latest release: %w", err)
 	}
+
 	if len(releases) == 0 {
 		return false, fmt.Errorf("no releases found")
 	}
+
 	for i := 0; i < len(releases); i++ {
 		if releases[i].TagName == version {
 			if i+1 < len(releases) {
