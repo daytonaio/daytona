bfe7fe7741ebab3289570bb578ea769a0bc7ceaa
Toma Puljak <toma.puljak@hotmail.com>
2025-01-10 13:18:06 +0000

    refactor: abstract api key generation methods (#1701)
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- pkg/server/apikeys/service.go
+++ pkg/server/apikeys/service.go
@@ -9,15 +9,21 @@ import (
 )
 
 type ApiKeyServiceConfig struct {
-	ApiKeyStore stores.ApiKeyStore
+	ApiKeyStore       stores.ApiKeyStore
+	GenerateRandomKey func(name string) string
+	GetKeyHash        func(key string) string
 }
 
 func NewApiKeyService(config ApiKeyServiceConfig) services.IApiKeyService {
 	return &ApiKeyService{
-		apiKeyStore: config.ApiKeyStore,
+		apiKeyStore:       config.ApiKeyStore,
+		generateRandomKey: config.GenerateRandomKey,
+		getKeyHash:        config.GetKeyHash,
 	}
 }
 
 type ApiKeyService struct {
-	apiKeyStore stores.ApiKeyStore
+	apiKeyStore       stores.ApiKeyStore
+	generateRandomKey func(name string) string
+	getKeyHash        func(key string) string
 }
