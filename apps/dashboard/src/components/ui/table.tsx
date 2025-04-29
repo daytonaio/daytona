a89a5214c6a53f6f067441a53e5d37a67cdcaddd
sudiptapaul <psudipta858@gmail.com>
2025-04-29 23:42:13 +0530

    signoff added
    Signed-off-by: sudiptapaul <psudipta858@gmail.com>

--- apps/dashboard/src/components/ui/table.tsx
+++ apps/dashboard/src/components/ui/table.tsx
@@ -77,4 +77,5 @@ const TableCaption = React.forwardRef<HTMLTableCaptionElement, React.HTMLAttribu
 )
 TableCaption.displayName = 'TableCaption'
 
+
 export { Table, TableHeader, TableBody, TableFooter, TableHead, TableRow, TableCell, TableCaption }
