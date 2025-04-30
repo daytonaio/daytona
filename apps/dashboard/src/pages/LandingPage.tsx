a0d1ec78d84258619f156426f33f9244602adf81
Fabjan Vučina <fabjanvucina@gmail.com>
2025-04-30 11:33:53 +0200

    fix(dashboard): move posthog user identification to App.tsx (#1800)
    Signed-off-by: fabjanvucina <fabjanvucina@gmail.com>

--- apps/dashboard/src/pages/LandingPage.tsx
+++ apps/dashboard/src/pages/LandingPage.tsx
@@ -3,24 +3,13 @@
  * SPDX-License-Identifier: AGPL-3.0
  */
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuth } from 'react-oidc-context'
 import LoadingFallback from '@/components/LoadingFallback'
-import { usePostHog } from 'posthog-js/react'
 
 const LandingPage: React.FC = () => {
-  const { signinRedirect, isAuthenticated, isLoading, user } = useAuth()
-  const posthog = usePostHog()
-
-  useEffect(() => {
-    if (import.meta.env.PROD && isAuthenticated && user && posthog?.get_distinct_id() !== user.profile.sub) {
-      posthog?.identify(user.profile.sub, {
-        email: user.profile.email,
-        name: user.profile.name,
-      })
-    }
-  }, [isAuthenticated, user, posthog])
+  const { signinRedirect, isAuthenticated, isLoading } = useAuth()
 
   if (isLoading) {
     return <LoadingFallback />
