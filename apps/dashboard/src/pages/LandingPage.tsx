5e21f3c9b4c136391920f2756028dacb29801c34
Ritik Mohapatra <87339903+OxDTH@users.noreply.github.com>
2025-05-05 19:10:26 +0530

    refactor(dashboard): implement route management with RoutePath enum (#1786)
    Signed-off-by: OxDTH <ritikmohapatra2017@gmail.com>

--- apps/dashboard/src/pages/LandingPage.tsx
+++ apps/dashboard/src/pages/LandingPage.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuth } from 'react-oidc-context'
 import LoadingFallback from '@/components/LoadingFallback'
+import { RoutePath } from '@/enums/RoutePath'
 
 const LandingPage: React.FC = () => {
   const { signinRedirect, isAuthenticated, isLoading } = useAuth()
@@ -16,7 +17,7 @@ const LandingPage: React.FC = () => {
   }
 
   if (isAuthenticated) {
-    return <Navigate to="/dashboard" replace />
+    return <Navigate to={RoutePath.DASHBOARD} replace />
   } else {
     void signinRedirect({
       state: {
