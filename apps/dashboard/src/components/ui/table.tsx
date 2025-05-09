3deac7a7c04e31219becff9d6b7bdff4b4559a7b
fabjanvucina <fabjanvucina@gmail.com>
2025-05-09 12:30:04 +0000

    fix(dashboard): table cell padding
    Signed-off-by: fabjanvucina <fabjanvucina@gmail.com>

--- apps/dashboard/src/components/ui/table.tsx
+++ apps/dashboard/src/components/ui/table.tsx
@@ -50,10 +50,7 @@ const TableHead = React.forwardRef<HTMLTableCellElement, React.ThHTMLAttributes<
   ({ className, ...props }, ref) => (
     <th
       ref={ref}
-      className={cn(
-        'h-8 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0',
-        className,
-      )}
+      className={cn('h-8 px-2 text-left align-middle font-medium text-muted-foreground', className)}
       {...props}
     />
   ),
@@ -61,9 +58,7 @@ const TableHead = React.forwardRef<HTMLTableCellElement, React.ThHTMLAttributes<
 TableHead.displayName = 'TableHead'
 
 const TableCell = React.forwardRef<HTMLTableCellElement, React.TdHTMLAttributes<HTMLTableCellElement>>(
-  ({ className, ...props }, ref) => (
-    <td ref={ref} className={cn('p-2 align-middle [&:has([role=checkbox])]:pr-0', className)} {...props} />
-  ),
+  ({ className, ...props }, ref) => <td ref={ref} className={cn('p-2 align-middle', className)} {...props} />,
 )
 TableCell.displayName = 'TableCell'
 
