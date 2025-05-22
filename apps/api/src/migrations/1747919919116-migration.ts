ad98ded63666e46bc54fabfc13a72a925690f28d
Ivan Dagelic <dagelic.ivan@gmail.com>
2025-05-22 13:45:31 +0000

    fix: migration type rename
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- apps/api/src/migrations/1747919919116-migration.ts
+++ apps/api/src/migrations/1747919919116-migration.ts
@@ -8,13 +8,11 @@ export class Migration1747919919116 implements MigrationInterface {
     await queryRunner.renameColumn('workspace', 'snapshotImage', 'backupImage')
     await queryRunner.renameColumn('workspace', 'lastSnapshotAt', 'lastBackupAt')
     await queryRunner.renameColumn('workspace', 'snapshotState', 'backupState')
-    await queryRunner.query(`ALTER TYPE "public"."workspace_snapshotstate_enum" RENAME TO "workspace_backupstate_enum"`)
     await queryRunner.renameColumn('workspace', 'existingSnapshotImages', 'existingBackupImages')
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.renameColumn('workspace', 'existingBackupImages', 'existingSnapshotImages')
-    await queryRunner.query(`ALTER TYPE "public"."workspace_backupstate_enum" RENAME TO "workspace_snapshotstate_enum"`)
     await queryRunner.renameColumn('workspace', 'backupState', 'snapshotState')
     await queryRunner.renameColumn('workspace', 'lastBackupAt', 'lastSnapshotAt')
     await queryRunner.renameColumn('workspace', 'backupImage', 'snapshotImage')
