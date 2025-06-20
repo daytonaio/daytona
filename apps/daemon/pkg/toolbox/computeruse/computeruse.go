11aa82365acbef3cf7dcad8a9c7e250d2e611c39
Vedran <vedran.jukic@gmail.com>
2025-06-20 09:39:02 +0000

    feat: computer use api
    Signed-off-by: Vedran <vedran.jukic@gmail.com>

---
+++ apps/daemon/pkg/toolbox/computeruse/computeruse.go
@@ -0,0 +1,12 @@
+// Copyright 2025 Daytona Platforms Inc.
+// SPDX-License-Identifier: AGPL-3.0
+
+package computeruse
+
+type ComputerUse struct {
+	// Add any agent state here
+}
+
+func NewComputerUse() *ComputerUse {
+	return &ComputerUse{}
+}
