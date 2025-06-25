dcf3137ba7fa7533e954dc12248204304963db3f
Vedran <vedran.jukic@gmail.com>
2025-06-25 15:37:17 +0000

    rename plugin path
    Signed-off-by: Vedran <vedran.jukic@gmail.com>

--- apps/runner/pkg/docker/container_configs.go
+++ apps/runner/pkg/docker/container_configs.go
@@ -55,8 +55,8 @@ func (d *DockerClient) getContainerHostConfig(ctx context.Context, sandboxDto dt
 	binds = append(binds, fmt.Sprintf("%s:/usr/local/bin/daytona:ro", d.daemonPath))
 
 	// Mount the plugin if available
-	if d.pluginPath != "" {
-		binds = append(binds, fmt.Sprintf("%s:/usr/local/lib/daytona-computer-use:ro", d.pluginPath))
+	if d.computerUsePluginPath != "" {
+		binds = append(binds, fmt.Sprintf("%s:/usr/local/lib/daytona-computer-use:ro", d.computerUsePluginPath))
 	}
 
 	if len(volumeMountPathBinds) > 0 {
