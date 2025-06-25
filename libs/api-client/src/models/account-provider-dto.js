c98d4a072884ce0efca246ac5eba9769ea515683
MDzaja <mirkodzaja0@gmail.com>
2025-06-25 08:24:54 +0000

    fix: pr checks; remove unnecessary files form api clients
    Signed-off-by: MDzaja <mirkodzaja0@gmail.com>

--- libs/api-client/src/models/account-provider-dto.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-/* tslint:disable */
-Object.defineProperty(exports, '__esModule', { value: true })
-//# sourceMappingURL=account-provider-dto.js.map
--- libs/api-client/src/models/get-build-logs400-response.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-/* tslint:disable */
-Object.defineProperty(exports, '__esModule', { value: true })
-//# sourceMappingURL=get-build-logs400-response.js.map
