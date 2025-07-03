f2a06bbf3400d928196fb4f36133d060ea211838
lukaivanovic <lluka.ivanovic@gmail.com>
2025-07-03 20:14:05 +0200

    feat: sandbox redesign
    Signed-off-by: lukaivanovic <lluka.ivanovic@gmail.com>

--- apps/dashboard/src/components/ui/command.tsx
+++ apps/dashboard/src/components/ui/command.tsx
@@ -42,14 +42,11 @@ const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
 >(({ className, ...props }, ref) => (
-  <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
+  <div className={cn('flex items-center border-b px-3', className)}>
     <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
     <CommandPrimitive.Input
       ref={ref}
-      className={cn(
-        'flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
+      className="flex h-8 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
       {...props}
     />
   </div>
--- apps/dashboard/src/components/ui/table.tsx
+++ apps/dashboard/src/components/ui/table.tsx
@@ -9,7 +9,7 @@ import { cn } from '@/lib/utils'
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
-    <div className="relative w-full overflow-auto">
+    <div className="flex-1 w-full overflow-auto">
       <table ref={ref} className={cn('w-full caption-bottom text-sm', className)} {...props} />
     </div>
   ),
@@ -23,7 +23,7 @@ TableHeader.displayName = 'TableHeader'
 
 const TableBody = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
   ({ className, ...props }, ref) => (
-    <tbody ref={ref} className={cn('[&_tr:last-child]:border-0', '[&_tr]:h-14', className)} {...props} />
+    <tbody ref={ref} className={cn('[&_tr:last-child]:border-0', '[&_tr]:h-10', className)} {...props} />
   ),
 )
 TableBody.displayName = 'TableBody'
@@ -39,30 +39,46 @@ const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTML
   ({ className, ...props }, ref) => (
     <tr
       ref={ref}
-      className={cn('border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted', className)}
-      {...props}
-    />
-  ),
-)
-TableRow.displayName = 'TableRow'
-
-const TableHead = React.forwardRef<HTMLTableCellElement, React.ThHTMLAttributes<HTMLTableCellElement>>(
-  ({ className, ...props }, ref) => (
-    <th
-      ref={ref}
       className={cn(
-        'h-8 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:w-10',
+        'bg-background border-b transition-colors data-[state=selected]:bg-muted/25 dark:data-[state=selected]:bg-muted/25',
         className,
       )}
       {...props}
     />
   ),
 )
+TableRow.displayName = 'TableRow'
+
+const TableHead = React.forwardRef<
+  HTMLTableCellElement,
+  React.ThHTMLAttributes<HTMLTableCellElement> & { sticky?: 'left' | 'right' }
+>(({ className, sticky, ...props }, ref) => (
+  <th
+    ref={ref}
+    className={cn(
+      'bg-inherit h-10 px-2 text-left align-middle font-medium [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
+      sticky && cn('sticky', 'z-10', sticky === 'left' ? 'left-0' : 'right-0'),
+      className,
+    )}
+    {...props}
+  />
+))
 TableHead.displayName = 'TableHead'
 
-const TableCell = React.forwardRef<HTMLTableCellElement, React.TdHTMLAttributes<HTMLTableCellElement>>(
-  ({ className, ...props }, ref) => <td ref={ref} className={cn('p-2 align-middle', className)} {...props} />,
-)
+const TableCell = React.forwardRef<
+  HTMLTableCellElement,
+  React.TdHTMLAttributes<HTMLTableCellElement> & { sticky?: 'left' | 'right' }
+>(({ className, sticky, ...props }, ref) => (
+  <td
+    ref={ref}
+    className={cn(
+      'bg-inherit p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
+      sticky && cn('sticky', 'z-[1]', sticky === 'left' ? 'left-0' : 'right-0'),
+      className,
+    )}
+    {...props}
+  />
+))
 TableCell.displayName = 'TableCell'
 
 const TableCaption = React.forwardRef<HTMLTableCaptionElement, React.HTMLAttributes<HTMLTableCaptionElement>>(
