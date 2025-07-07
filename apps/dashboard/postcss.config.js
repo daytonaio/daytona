f57bb9b9705f6f49cfebce7fb666b6bc6de06aaf
Toma Puljak <toma.puljak@hotmail.com>
2025-07-07 08:45:16 +0000

    fix(dashboard): revert tailwindcss to 3.4.3
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- apps/dashboard/postcss.config.js
+++ apps/dashboard/postcss.config.js
@@ -12,7 +12,7 @@ const { join } = require('path')
 
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {
+    tailwindcss: {
       config: join(__dirname, 'tailwind.config.js'),
     },
     autoprefixer: {},
