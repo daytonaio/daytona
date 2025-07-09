401b04d37f6576381e02724894fa737f591fc032
Fabjan Vučina <fabjanvucina@gmail.com>
2025-07-09 17:15:21 +0200

    refactor(api): check email verified when creating personal org (#2129)
    Signed-off-by: fabjanvucina <fabjanvucina@gmail.com>

--- apps/api/src/user/events/user-created.event.ts
+++ apps/api/src/user/events/user-created.event.ts
@@ -4,14 +4,13 @@
  */
 
 import { EntityManager } from 'typeorm'
+import { User } from '../user.entity'
 import { CreateOrganizationQuotaDto } from '../../organization/dto/create-organization-quota.dto'
 
 export class UserCreatedEvent {
   constructor(
     public readonly entityManager: EntityManager,
-    public readonly userId: string,
-    public readonly email?: string,
+    public readonly user: User,
     public readonly personalOrganizationQuota?: CreateOrganizationQuotaDto,
-    public readonly emailVerified?: boolean,
   ) {}
 }
