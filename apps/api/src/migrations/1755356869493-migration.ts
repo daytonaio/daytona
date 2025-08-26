e558aa2466c6211111e0233e94991fd240770872
fabjanvucina <fabjanvucina@gmail.com>
2025-08-26 16:17:37 +0000

    fix: cleanup migration
    Signed-off-by: fabjanvucina <fabjanvucina@gmail.com>

--- apps/api/src/migrations/1755356869493-migration.ts
+++ apps/api/src/migrations/1755356869493-migration.ts
@@ -5,7 +5,7 @@ export class Migration1755356869493 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "webhook_initialization" ("organizationId" character varying NOT NULL, "endpointsCreated" boolean NOT NULL DEFAULT false, "svixApplicationCreated" boolean NOT NULL DEFAULT false, "endpointIds" jsonb, "svixApplicationId" character varying, "lastError" text, "retryCount" integer NOT NULL DEFAULT '0', "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "webhook_initialization_organizationId_pk" PRIMARY KEY ("organizationId"))`,
+      `CREATE TABLE "webhook_initialization" ("organizationId" character varying NOT NULL, "svixApplicationId" character varying, "lastError" text, "retryCount" integer NOT NULL DEFAULT '0', "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "webhook_initialization_organizationId_pk" PRIMARY KEY ("organizationId"))`,
     )
   }
 
