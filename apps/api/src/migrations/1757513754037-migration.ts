642b553e2aae4bc4c8489f3b46fb2c18a1b4d38b
Ivan Dagelic <dagelic.ivan@gmail.com>
2025-09-19 14:22:01 +0000

    fix: clone internal to backup registry migration
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- apps/api/src/migrations/1757513754037-migration.ts
+++ apps/api/src/migrations/1757513754037-migration.ts
@@ -23,9 +23,38 @@ export class Migration1757513754037 implements MigrationInterface {
     )
     await queryRunner.query(`ALTER TABLE "docker_registry" ALTER COLUMN "registryType" SET DEFAULT 'internal'`)
     await queryRunner.query(`DROP TYPE "public"."docker_registry_registrytype_enum_old"`)
+
+    // Create the base default registry by copying from the default internal one
+    await queryRunner.query(`
+          INSERT INTO public.docker_registry (
+            name, url, username, password, "isDefault", project, "createdAt", "updatedAt", "registryType", "organizationId", "region"
+          )
+          SELECT
+            'Backup Registry' AS name,
+            url,
+            username,
+            password,
+            "isDefault",
+            project,
+            now() AS "createdAt",
+            now() AS "updatedAt",
+            'backup' AS "registryType",
+            "organizationId",
+            "region"
+          FROM public.docker_registry
+          WHERE "registryType" = 'internal'
+            AND "isDefault" = true
+          ORDER BY "createdAt" ASC
+          LIMIT 1
+        `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      DELETE FROM public.docker_registry
+      WHERE "registryType" = 'backup'
+    `)
+
     await queryRunner.query(
       `CREATE TYPE "public"."docker_registry_registrytype_enum_old" AS ENUM('internal', 'organization', 'transient', 'backup')`,
     )
