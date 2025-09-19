f56ad936d696c2b572c94988ec0651e87a2bd573
Ivan Dagelic <dagelic.ivan@gmail.com>
2025-09-19 14:18:12 +0000

    fix: migration
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- apps/api/src/migrations/1757513754037-migration.ts
+++ apps/api/src/migrations/1757513754037-migration.ts
@@ -11,29 +11,33 @@ export class Migration1757513754037 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "docker_registry" ADD "region" character varying`)
 
-    await queryRunner.query(`ALTER TYPE "public"."registry_type_enum" RENAME TO "registry_type_enum_old"`)
     await queryRunner.query(
-      `CREATE TYPE "public"."registry_type_enum" AS ENUM('internal', 'organization', 'transient', 'backup')`,
+      `ALTER TYPE "public"."docker_registry_registrytype_enum" RENAME TO "docker_registry_registrytype_enum_old"`,
+    )
+    await queryRunner.query(
+      `CREATE TYPE "public"."docker_registry_registrytype_enum" AS ENUM('internal', 'organization', 'transient', 'backup')`,
     )
     await queryRunner.query(`ALTER TABLE "docker_registry" ALTER COLUMN "registryType" DROP DEFAULT`)
     await queryRunner.query(
-      `ALTER TABLE "docker_registry" ALTER COLUMN "registryType" TYPE "public"."registry_type_enum" USING "registryType"::"text"::"public"."registry_type_enum"`,
+      `ALTER TABLE "docker_registry" ALTER COLUMN "registryType" TYPE "public"."docker_registry_registrytype_enum" USING "registryType"::"text"::"public"."docker_registry_registrytype_enum"`,
     )
     await queryRunner.query(`ALTER TABLE "docker_registry" ALTER COLUMN "registryType" SET DEFAULT 'internal'`)
-    await queryRunner.query(`DROP TYPE "public"."registry_type_enum_old"`)
+    await queryRunner.query(`DROP TYPE "public"."docker_registry_registrytype_enum_old"`)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TYPE "public"."registry_type_enum_old" AS ENUM('internal', 'organization', 'transient', 'backup')`,
+      `CREATE TYPE "public"."docker_registry_registrytype_enum_old" AS ENUM('internal', 'organization', 'transient', 'backup')`,
     )
     await queryRunner.query(`ALTER TABLE "docker_registry" ALTER COLUMN "registryType" DROP DEFAULT`)
     await queryRunner.query(
-      `ALTER TABLE "docker_registry" ALTER COLUMN "registryType" TYPE "public"."registry_type_enum_old" USING "registryType"::"text"::"public"."registry_type_enum_old"`,
+      `ALTER TABLE "docker_registry" ALTER COLUMN "registryType" TYPE "public"."docker_registry_registrytype_enum_old" USING "registryType"::"text"::"public"."docker_registry_registrytype_enum_old"`,
     )
     await queryRunner.query(`ALTER TABLE "docker_registry" ALTER COLUMN "registryType" SET DEFAULT 'internal'`)
-    await queryRunner.query(`DROP TYPE "public"."registry_type_enum"`)
-    await queryRunner.query(`ALTER TYPE "public"."registry_type_enum_old" RENAME TO "registry_type_enum"`)
+    await queryRunner.query(`DROP TYPE "public"."docker_registry_registrytype_enum"`)
+    await queryRunner.query(
+      `ALTER TYPE "public"."docker_registry_registrytype_enum_old" RENAME TO "docker_registry_registrytype_enum"`,
+    )
 
     await queryRunner.query(`ALTER TABLE "docker_registry" DROP COLUMN "region"`)
   }
