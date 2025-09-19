002fdc98b4bf7969055b4c8192a4de4a40f2983f
Fabjan Vučina <fabjanvucina@gmail.com>
2025-09-19 13:47:42 +0200

    fix(api): rollback pending quota usage in case of error, sandbox start lock, snapshot activate lock  (#2441)
    Signed-off-by: fabjanvucina <fabjanvucina@gmail.com>

--- apps/api/src/organization/constants/volume-usage-ignored-states.constant.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2025 Daytona Platforms Inc.
- * SPDX-License-Identifier: AGPL-3.0
- */
-
-import { VolumeState } from '../../sandbox/enums/volume-state.enum'
-
-export const VOLUME_USAGE_IGNORED_STATES: VolumeState[] = [VolumeState.DELETED, VolumeState.ERROR]
