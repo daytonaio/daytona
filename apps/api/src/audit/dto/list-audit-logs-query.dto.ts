354b0e3308d15054821cb18bba49919fa9d25c50
fabjanvucina <fabjanvucina@gmail.com>
2025-09-26 09:22:28 +0000

    refactor(api): page limit and page number decorators
    Signed-off-by: fabjanvucina <fabjanvucina@gmail.com>

--- apps/api/src/audit/dto/list-audit-logs-query.dto.ts
+++ apps/api/src/audit/dto/list-audit-logs-query.dto.ts
@@ -3,39 +3,15 @@
  * SPDX-License-Identifier: AGPL-3.0
  */
 
-import { ApiProperty, ApiSchema } from '@nestjs/swagger'
-import { IsInt, IsOptional, Min, Max } from 'class-validator'
-import { Type } from 'class-transformer'
+import { ApiSchema } from '@nestjs/swagger'
+import { PageNumber } from '../../common/decorators/page-number.decorator'
+import { PageLimit } from '../../common/decorators/page-limit.decorator'
 
 @ApiSchema({ name: 'ListAuditLogsQuery' })
 export class ListAuditLogsQueryDto {
-  @ApiProperty({
-    name: 'page',
-    description: 'Page number of the results',
-    required: false,
-    type: Number,
-    minimum: 1,
-    default: 1,
-  })
-  @IsOptional()
-  @Type(() => Number)
-  @IsInt()
-  @Min(1)
+  @PageNumber(1)
   page = 1
 
-  @ApiProperty({
-    name: 'limit',
-    description: 'Number of results per page',
-    required: false,
-    type: Number,
-    minimum: 1,
-    maximum: 100,
-    default: 10,
-  })
-  @IsOptional()
-  @Type(() => Number)
-  @IsInt()
-  @Min(1)
-  @Max(100)
+  @PageLimit(10)
   limit = 10
 }
