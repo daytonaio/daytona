48df620c3db371a2f3f55b696e3bb909feeabdaa
Luka Brecic <luka.brecic3@gmail.com>
2025-09-30 14:17:39 +0000

    feat(api): add migration to drop used and capacity
    Signed-off-by: Luka Brecic <luka.brecic3@gmail.com>

---
+++ apps/api/src/migrations/1759241690773-migration.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class Migration1759241690773 implements MigrationInterface {
+  name = 'Migration1759241690773'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "runner" DROP COLUMN "used"`)
+    await queryRunner.query(`ALTER TABLE "runner" DROP COLUMN "capacity"`)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "runner" ADD "capacity" integer NOT NULL`)
+    await queryRunner.query(`ALTER TABLE "runner" ADD "used" integer NOT NULL DEFAULT '0'`)
+  }
+}
