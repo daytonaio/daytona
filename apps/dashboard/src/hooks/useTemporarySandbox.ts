08681983271d5914a75020fac12ae5075e93254c
Mislav Ivanda <mislavivanda454@gmail.com>
2025-10-04 15:58:52 +0000

    useTemporarySandbox license added

--- apps/dashboard/src/hooks/useTemporarySandbox.ts
+++ apps/dashboard/src/hooks/useTemporarySandbox.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2025 Daytona Platforms Inc.
+ * SPDX-License-Identifier: AGPL-3.0
+ */
+
 import { useEffect, useRef, useCallback, useState } from 'react'
 import { usePlayground } from '@/hooks/usePlayground'
 import { Sandbox } from '@daytonaio/sdk'
