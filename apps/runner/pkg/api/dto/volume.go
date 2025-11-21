f2c156c0941eb528f3bb481dd39a0a7ae0355b17
Vedran <vedran.jukic@gmail.com>
2025-11-21 14:18:04 +0000

    fix: optional param
    Signed-off-by: Vedran <vedran.jukic@gmail.com>

--- apps/runner/pkg/api/dto/volume.go
+++ apps/runner/pkg/api/dto/volume.go
@@ -4,7 +4,7 @@
 package dto
 
 type VolumeDTO struct {
-	VolumeId  string `json:"volumeId"`
-	MountPath string `json:"mountPath"`
-	Subpath   string `json:"subpath,omitempty"`
+	VolumeId  string  `json:"volumeId"`
+	MountPath string  `json:"mountPath"`
+	Subpath   *string `json:"subpath,omitempty"`
 }
