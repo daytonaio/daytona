7b83d05e9b285bbaf1e5030d4f6a4491c801e35b
Ivan Dagelic <dagelic.ivan@gmail.com>
2025-11-21 15:52:28 +0100

    feat(cli): daytona sandbox archive command (#2953)
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

---
+++ apps/cli/cmd/sandbox/archive.go
@@ -0,0 +1,40 @@
+// Copyright 2025 Daytona Platforms Inc.
+// SPDX-License-Identifier: AGPL-3.0
+
+package sandbox
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/daytonaio/daytona/cli/apiclient"
+	view_common "github.com/daytonaio/daytona/cli/views/common"
+	"github.com/spf13/cobra"
+)
+
+var ArchiveCmd = &cobra.Command{
+	Use:   "archive [SANDBOX_ID] | [SANDBOX_NAME]",
+	Short: "Archive a sandbox",
+	Args:  cobra.MaximumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := context.Background()
+
+		apiClient, err := apiclient.GetApiClient(nil, nil)
+		if err != nil {
+			return err
+		}
+
+		sandboxIdOrNameArg := args[0]
+
+		_, res, err := apiClient.SandboxAPI.ArchiveSandbox(ctx, sandboxIdOrNameArg).Execute()
+		if err != nil {
+			return apiclient.HandleErrorResponse(res, err)
+		}
+
+		view_common.RenderInfoMessageBold(fmt.Sprintf("Sandbox %s marked for archival", sandboxIdOrNameArg))
+		return nil
+	},
+}
+
+func init() {
+}
