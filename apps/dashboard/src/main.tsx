b6a92f20c837ae0a04320787a3a26f6695a0cee1
rpavlini <rpavlinic@daytona.io>
2025-11-25 15:16:44 +0000

    feat(dashboard); scaffold msw setup
    Signed-off-by: rpavlini <rpavlinic@daytona.io>

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -5,26 +5,37 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { PostHogProviderWrapper } from './components/PostHogProviderWrapper'
-import App from './App'
-import './index.css'
 import { ErrorBoundary } from 'react-error-boundary'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import { ErrorBoundaryFallback } from './components/ErrorBoundaryFallback'
+import { PostHogProviderWrapper } from './components/PostHogProviderWrapper'
+import './index.css'
 import { ConfigProvider } from './providers/ConfigProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(
-  <React.StrictMode>
-    <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
-      <ConfigProvider>
-        <PostHogProviderWrapper>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </PostHogProviderWrapper>
-      </ConfigProvider>
-    </ErrorBoundary>
-  </React.StrictMode>,
+async function enableMocking() {
+  if (import.meta.env.VITE_ENABLE_MOCKING !== 'true') {
+    return
+  }
+
+  const { worker } = await import('./mocks/browser')
+  return worker.start()
+}
+
+enableMocking().then(() =>
+  root.render(
+    <React.StrictMode>
+      <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
+        <ConfigProvider>
+          <PostHogProviderWrapper>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </PostHogProviderWrapper>
+        </ConfigProvider>
+      </ErrorBoundary>
+    </React.StrictMode>,
+  ),
 )
