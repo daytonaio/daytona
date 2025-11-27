4c55a1e62bdbf284581aa0c1b19979dc15e389ba
fabjanvucina <fabjanvucina@gmail.com>
2025-11-27 15:25:06 +0000

    fix: migration
    Signed-off-by: fabjanvucina <fabjanvucina@gmail.com>

--- apps/api/src/migrations/1764073472179-migration.ts
+++ apps/api/src/migrations/1764073472179-migration.ts
@@ -51,21 +51,6 @@ export class Migration1764073472179 implements MigrationInterface {
               o."total_disk_quota"
             FROM "organization" o
           `)
-
-    // For all other regions where quotas are enforced, assign default quotas from configuration
-    await queryRunner.query(`
-            INSERT INTO "region_quota" ("organizationId", "regionId", "total_cpu_quota", "total_memory_quota", "total_disk_quota")
-            SELECT 
-              o."id" as "organizationId",
-              r."id" as "regionId",
-              ${configuration.defaultOrganizationQuota.totalCpuQuota} as "total_cpu_quota",
-              ${configuration.defaultOrganizationQuota.totalMemoryQuota} as "total_memory_quota", 
-              ${configuration.defaultOrganizationQuota.totalDiskQuota} as "total_disk_quota"
-            FROM "organization" o
-            CROSS JOIN "region" r
-            WHERE r."enforceQuotas" = true 
-              AND r."id" != o."defaultRegionId"
-          `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
