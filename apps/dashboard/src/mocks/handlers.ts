1fedcd2ad9208ece7da280476067c8eb9505a673
rpavlini <rpavlinic@daytona.io>
2025-12-05 11:51:54 +0000

    chore: rm log
    Signed-off-by: rpavlini <rpavlinic@daytona.io>

--- apps/dashboard/src/mocks/handlers.ts
+++ apps/dashboard/src/mocks/handlers.ts
@@ -14,7 +14,6 @@ const API_URL = import.meta.env.VITE_API_URL
 export const handlers = [
   http.get(`${API_URL}/config`, async () => {
     const originalConfig = await fetch(bypass(`${API_URL}/config`)).then((res) => res.json())
-    console.log(originalConfig)
 
     return HttpResponse.json<Partial<DaytonaConfiguration>>({
       ...originalConfig,
