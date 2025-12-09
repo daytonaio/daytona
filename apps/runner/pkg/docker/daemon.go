9b04c5326bb1e271b4d8f7e53fc077600601be63
Luka Brecic <luka.brecic3@gmail.com>
2025-12-09 09:14:26 +0100

    feat(runner,daemon): daemon sandbox entrypoint (#2803)
    Signed-off-by: Luka Brecic <luka.brecic3@gmail.com>
    Signed-off-by: Goran Draganic <goran@daytona.io>
    Co-authored-by: Goran Draganic <goran@daytona.io>

---
+++ apps/runner/pkg/docker/daemon.go
@@ -0,0 +1,79 @@
+// Copyright 2025 Daytona Platforms Inc.
+// SPDX-License-Identifier: AGPL-3.0
+
+package docker
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/url"
+	"time"
+
+	common_errors "github.com/daytonaio/common-go/pkg/errors"
+	"github.com/daytonaio/common-go/pkg/timer"
+	"github.com/docker/docker/api/types/container"
+)
+
+func (d *DockerClient) startDaytonaDaemon(ctx context.Context, containerId string, workDir string) error {
+	defer timer.Timer()()
+
+	var envVars []string
+	if workDir == "" {
+		envVars = append(envVars, "DAYTONA_USER_HOME_AS_WORKDIR=true")
+	}
+
+	execOptions := container.ExecOptions{
+		Cmd:          []string{"/usr/local/bin/daytona"},
+		AttachStdout: true,
+		AttachStderr: true,
+		WorkingDir:   workDir,
+		Env:          envVars,
+		Tty:          true,
+	}
+
+	execStartOptions := container.ExecStartOptions{
+		Detach: false,
+	}
+
+	result, err := d.execSync(ctx, containerId, execOptions, execStartOptions)
+	if err != nil {
+		return err
+	}
+
+	if result.ExitCode != 0 {
+		return fmt.Errorf("failed to start daytona daemon with exit code %d: %s", result.ExitCode, result.StdErr)
+	}
+
+	return nil
+}
+
+func (d *DockerClient) waitForDaemonRunning(ctx context.Context, containerIP string) error {
+	defer timer.Timer()()
+
+	// Build the target URL
+	targetURL := fmt.Sprintf("http://%s:2280/version", containerIP)
+	target, err := url.Parse(targetURL)
+	if err != nil {
+		return common_errors.NewBadRequestError(fmt.Errorf("failed to parse target URL: %w", err))
+	}
+
+	timeout := time.Duration(d.daemonStartTimeoutSec) * time.Second
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	for {
+		select {
+		case <-timeoutCtx.Done():
+			return fmt.Errorf("timeout waiting for daemon to start")
+		default:
+			conn, err := net.DialTimeout("tcp", target.Host, 1*time.Second)
+			if err != nil {
+				time.Sleep(5 * time.Millisecond)
+				continue
+			}
+			conn.Close()
+			return nil
+		}
+	}
+}
