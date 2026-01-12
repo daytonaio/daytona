da567e5322e26bca6e8d7b5644aa23a2e3fe9535
Juraj Štefanić <80431202+stefanicjuraj@users.noreply.github.com>
2026-01-12 15:26:38 +0100

    feat(docs): add api reference component (#3333)
    Signed-off-by: Juraj Štefanić <jurajstefanic@outlook.com>

--- libs/toolbox-api-client/src/api/info-api.ts
+++ libs/toolbox-api-client/src/api/info-api.ts
@@ -1,8 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
- * Daytona Daemon API
- * Daytona Daemon API
+ * Daytona Toolbox API
+ * Daytona Toolbox API
  *
  * The version of the OpenAPI document: v0.0.0-dev
  *
