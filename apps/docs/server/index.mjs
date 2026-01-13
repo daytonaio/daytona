e5ef0cddf35a1840bb7e50aab11346988ed2b66d
Juraj Štefanić <80431202+stefanicjuraj@users.noreply.github.com>
2026-01-13 13:45:40 +0100

    fix(docs): modify file paths server configuration (#3351)
    Signed-off-by: Juraj Štefanić <jurajstefanic@outlook.com>

--- apps/docs/server/index.mjs
+++ apps/docs/server/index.mjs
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { handler as ssrHandler } from '../dist/server/entry.mjs'
+import { handler as ssrHandler } from '../server/entry.mjs'
 import { env } from './util/environment.mjs'
 
 const app = express()
@@ -9,10 +9,10 @@ app.use((req, res, next) => {
   res.setHeader('X-Frame-Options', 'SAMEORIGIN')
   next()
 })
-app.use('/docs', express.static('dist/client/'))
+app.use('/docs', express.static('client/'))
 app.use(ssrHandler)
 app.use((req, res) => {
-  res.sendFile('404.html', { root: 'dist/client/' })
+  res.sendFile('404.html', { root: 'client/' })
 })
 
 app.listen(env.FUNCTIONS_PORT, () => {
