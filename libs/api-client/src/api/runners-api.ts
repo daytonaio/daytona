c31cefb345203bbe52e270a8b926dee6eee1d326
fabjanvucina <fabjanvucina@gmail.com>
2026-01-15 13:47:19 +0000

    chore(api): remove obsolete getInitialRunnerBySnapshotId endpoint
    Signed-off-by: fabjanvucina <fabjanvucina@gmail.com>

--- libs/api-client/src/api/runners-api.ts
+++ libs/api-client/src/api/runners-api.ts
@@ -178,49 +178,6 @@ export const RunnersApiAxiosParamCreator = function (configuration?: Configurati
     },
     /**
      *
-     * @summary Get initial runner by snapshot ID
-     * @param {string} snapshotId Snapshot ID
-     * @param {*} [options] Override http request option.
-     * @throws {RequiredError}
-     */
-    getInitialRunnerBySnapshotId: async (
-      snapshotId: string,
-      options: RawAxiosRequestConfig = {},
-    ): Promise<RequestArgs> => {
-      // verify required parameter 'snapshotId' is not null or undefined
-      assertParamExists('getInitialRunnerBySnapshotId', 'snapshotId', snapshotId)
-      const localVarPath = `/runners/inital-by-snapshot-id/{snapshotId}`.replace(
-        `{${'snapshotId'}}`,
-        encodeURIComponent(String(snapshotId)),
-      )
-      // use dummy base URL string because the URL constructor only accepts absolute URLs.
-      const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL)
-      let baseOptions
-      if (configuration) {
-        baseOptions = configuration.baseOptions
-      }
-
-      const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options }
-      const localVarHeaderParameter = {} as any
-      const localVarQueryParameter = {} as any
-
-      // authentication bearer required
-      // http bearer authentication required
-      await setBearerAuthToObject(localVarHeaderParameter, configuration)
-
-      // authentication oauth2 required
-
-      setSearchParams(localVarUrlObj, localVarQueryParameter)
-      let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {}
-      localVarRequestOptions.headers = { ...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers }
-
-      return {
-        url: toPathString(localVarUrlObj),
-        options: localVarRequestOptions,
-      }
-    },
-    /**
-     *
      * @summary Get runner by ID
      * @param {string} id Runner ID
      * @param {string} [xDaytonaOrganizationID] Use with JWT to specify the organization ID
@@ -595,29 +552,6 @@ export const RunnersApiFp = function (configuration?: Configuration) {
     },
     /**
      *
-     * @summary Get initial runner by snapshot ID
-     * @param {string} snapshotId Snapshot ID
-     * @param {*} [options] Override http request option.
-     * @throws {RequiredError}
-     */
-    async getInitialRunnerBySnapshotId(
-      snapshotId: string,
-      options?: RawAxiosRequestConfig,
-    ): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<RunnerFull>> {
-      const localVarAxiosArgs = await localVarAxiosParamCreator.getInitialRunnerBySnapshotId(snapshotId, options)
-      const localVarOperationServerIndex = configuration?.serverIndex ?? 0
-      const localVarOperationServerBasePath =
-        operationServerMap['RunnersApi.getInitialRunnerBySnapshotId']?.[localVarOperationServerIndex]?.url
-      return (axios, basePath) =>
-        createRequestFunction(
-          localVarAxiosArgs,
-          globalAxios,
-          BASE_PATH,
-          configuration,
-        )(axios, localVarOperationServerBasePath || basePath)
-    },
-    /**
-     *
      * @summary Get runner by ID
      * @param {string} id Runner ID
      * @param {string} [xDaytonaOrganizationID] Use with JWT to specify the organization ID
@@ -834,16 +768,6 @@ export const RunnersApiFactory = function (configuration?: Configuration, basePa
     },
     /**
      *
-     * @summary Get initial runner by snapshot ID
-     * @param {string} snapshotId Snapshot ID
-     * @param {*} [options] Override http request option.
-     * @throws {RequiredError}
-     */
-    getInitialRunnerBySnapshotId(snapshotId: string, options?: RawAxiosRequestConfig): AxiosPromise<RunnerFull> {
-      return localVarFp.getInitialRunnerBySnapshotId(snapshotId, options).then((request) => request(axios, basePath))
-    },
-    /**
-     *
      * @summary Get runner by ID
      * @param {string} id Runner ID
      * @param {string} [xDaytonaOrganizationID] Use with JWT to specify the organization ID
@@ -970,20 +894,6 @@ export class RunnersApi extends BaseAPI {
   public getInfoForAuthenticatedRunner(options?: RawAxiosRequestConfig) {
     return RunnersApiFp(this.configuration)
       .getInfoForAuthenticatedRunner(options)
-      .then((request) => request(this.axios, this.basePath))
-  }
-
-  /**
-   *
-   * @summary Get initial runner by snapshot ID
-   * @param {string} snapshotId Snapshot ID
-   * @param {*} [options] Override http request option.
-   * @throws {RequiredError}
-   * @memberof RunnersApi
-   */
-  public getInitialRunnerBySnapshotId(snapshotId: string, options?: RawAxiosRequestConfig) {
-    return RunnersApiFp(this.configuration)
-      .getInitialRunnerBySnapshotId(snapshotId, options)
       .then((request) => request(this.axios, this.basePath))
   }
 
