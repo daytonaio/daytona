95e972ee01c495034bd757c12547e15f2d734cc2
Ivan Dagelic <dagelic.ivan@gmail.com>
2026-01-16 13:57:45 +0100

    feat(api): skip anonymous rate limit for config (#3391)
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- apps/api/src/config/config.controller.ts
+++ apps/api/src/config/config.controller.ts
@@ -3,15 +3,13 @@
  * SPDX-License-Identifier: AGPL-3.0
  */
 
-import { Controller, Get, UseGuards } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { TypedConfigService } from './typed-config.service'
 import { ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger'
 import { ConfigurationDto } from './dto/configuration.dto'
-import { AnonymousRateLimitGuard } from '../common/guards/anonymous-rate-limit.guard'
 
 @ApiTags('config')
 @Controller('config')
-@UseGuards(AnonymousRateLimitGuard)
 export class ConfigController {
   constructor(private readonly configService: TypedConfigService) {}
 
