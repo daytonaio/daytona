214b9f6f5d2e80afe1084652874f927108338bc4
rpavlini <rpavlinic@daytona.io>
2026-01-22 21:17:59 +0000

    feat(dashboard): copy button component
    Signed-off-by: rpavlini <rpavlinic@daytona.io>

---
+++ apps/dashboard/src/hooks/useCopyToClipboard.tsx
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2025 Daytona Platforms Inc.
+ * SPDX-License-Identifier: AGPL-3.0
+ */
+
+import { useCallback, useState } from 'react'
+import { toast } from 'sonner'
+
+type CopyFn = (text: string) => Promise<boolean>
+
+export function useCopyToClipboard({ timeout = 2000 }: { timeout?: number } = {}): [string | null, CopyFn] {
+  const [copiedText, setCopiedText] = useState<string | null>(null)
+
+  const copy: CopyFn = useCallback(
+    async (text) => {
+      if (!navigator?.clipboard) {
+        toast.error('Clipboard not supported')
+        return false
+      }
+
+      try {
+        await navigator.clipboard.writeText(text)
+
+        setCopiedText(text)
+
+        if (timeout !== 0) {
+          setTimeout(() => {
+            setCopiedText(null)
+          }, timeout)
+        }
+
+        return true
+      } catch (error) {
+        console.error('Failed to copy to clipboard', error)
+        setCopiedText(null)
+
+        toast.error('Failed to copy to clipboard')
+
+        return false
+      }
+    },
+    [timeout],
+  )
+
+  return [copiedText, copy]
+}
