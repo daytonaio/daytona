1ae3cde615e7c4eb593d589aba5d295092e05ab4
Toma Puljak <toma.puljak@hotmail.com>
2026-02-02 09:36:30 +0000

    fix(computer-use): native build
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- hack/computer-use/build-computer-use-amd64.sh
+++ hack/computer-use/build-computer-use-amd64.sh
@@ -12,8 +12,7 @@ fi
 if [ "$(uname -m)" = "x86_64" ]; then
     echo "Building computer-use for amd64 architecture (native build)..."
     cd libs/computer-use
-    GONOSUMDB=github.com/daytonaio/daytona go work sync go build -o ../../dist/libs/computer-use-amd64 main.go
-    ls -lh ../../dist/libs/computer-use-amd64
+    GONOSUMDB=github.com/daytonaio/daytona go build -o ../../dist/libs/computer-use-amd64 main.go
     echo "Native build completed successfully"
     exit 0
 fi
