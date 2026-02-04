05f8d1f7b9de8a3210eae4444b9829874f59acb2
Bruno Grbavac <bruno.grbavac@outlook.com>
2026-02-04 13:03:22 +0000

    chore: generated api clients
    Signed-off-by: Bruno Grbavac <bruno.grbavac@outlook.com>

--- libs/api-client-ruby/lib/daytona_api_client/models/resize_sandbox.rb
+++ libs/api-client-ruby/lib/daytona_api_client/models/resize_sandbox.rb
@@ -14,44 +14,22 @@ require 'date'
 require 'time'
 
 module DaytonaApiClient
-  class UpdateSandboxStateDto
-    # The new state for the sandbox
-    attr_accessor :state
+  class ResizeSandbox
+    # CPU cores to allocate to the sandbox (minimum: 1)
+    attr_accessor :cpu
 
-    # Optional error message when reporting an error state
-    attr_accessor :error_reason
+    # Memory in GB to allocate to the sandbox (minimum: 1)
+    attr_accessor :memory
 
-    # Whether the sandbox is recoverable
-    attr_accessor :recoverable
-
-    class EnumAttributeValidator
-      attr_reader :datatype
-      attr_reader :allowable_values
-
-      def initialize(datatype, allowable_values)
-        @allowable_values = allowable_values.map do |value|
-          case datatype.to_s
-          when /Integer/i
-            value.to_i
-          when /Float/i
-            value.to_f
-          else
-            value
-          end
-        end
-      end
-
-      def valid?(value)
-        !value || allowable_values.include?(value)
-      end
-    end
+    # Disk space in GB to allocate to the sandbox (can only be increased)
+    attr_accessor :disk
 
     # Attribute mapping from ruby-style variable name to JSON key.
     def self.attribute_map
       {
-        :'state' => :'state',
-        :'error_reason' => :'errorReason',
-        :'recoverable' => :'recoverable'
+        :'cpu' => :'cpu',
+        :'memory' => :'memory',
+        :'disk' => :'disk'
       }
     end
 
@@ -68,9 +46,9 @@ module DaytonaApiClient
     # Attribute type mapping.
     def self.openapi_types
       {
-        :'state' => :'String',
-        :'error_reason' => :'String',
-        :'recoverable' => :'Boolean'
+        :'cpu' => :'Integer',
+        :'memory' => :'Integer',
+        :'disk' => :'Integer'
       }
     end
 
@@ -84,30 +62,28 @@ module DaytonaApiClient
     # @param [Hash] attributes Model attributes in the form of hash
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `DaytonaApiClient::UpdateSandboxStateDto` initialize method"
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DaytonaApiClient::ResizeSandbox` initialize method"
       end
 
       # check to see if the attribute exists and convert string to symbol for hash key
       acceptable_attribute_map = self.class.acceptable_attribute_map
       attributes = attributes.each_with_object({}) { |(k, v), h|
         if (!acceptable_attribute_map.key?(k.to_sym))
-          fail ArgumentError, "`#{k}` is not a valid attribute in `DaytonaApiClient::UpdateSandboxStateDto`. Please check the name to make sure it's valid. List of attributes: " + acceptable_attribute_map.keys.inspect
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DaytonaApiClient::ResizeSandbox`. Please check the name to make sure it's valid. List of attributes: " + acceptable_attribute_map.keys.inspect
         end
         h[k.to_sym] = v
       }
 
-      if attributes.key?(:'state')
-        self.state = attributes[:'state']
-      else
-        self.state = nil
+      if attributes.key?(:'cpu')
+        self.cpu = attributes[:'cpu']
       end
 
-      if attributes.key?(:'error_reason')
-        self.error_reason = attributes[:'error_reason']
+      if attributes.key?(:'memory')
+        self.memory = attributes[:'memory']
       end
 
-      if attributes.key?(:'recoverable')
-        self.recoverable = attributes[:'recoverable']
+      if attributes.key?(:'disk')
+        self.disk = attributes[:'disk']
       end
     end
 
@@ -116,8 +92,16 @@ module DaytonaApiClient
     def list_invalid_properties
       warn '[DEPRECATED] the `list_invalid_properties` method is obsolete'
       invalid_properties = Array.new
-      if @state.nil?
-        invalid_properties.push('invalid value for "state", state cannot be nil.')
+      if !@cpu.nil? && @cpu < 1
+        invalid_properties.push('invalid value for "cpu", must be greater than or equal to 1.')
+      end
+
+      if !@memory.nil? && @memory < 1
+        invalid_properties.push('invalid value for "memory", must be greater than or equal to 1.')
+      end
+
+      if !@disk.nil? && @disk < 1
+        invalid_properties.push('invalid value for "disk", must be greater than or equal to 1.')
       end
 
       invalid_properties
@@ -127,20 +111,52 @@ module DaytonaApiClient
     # @return true if the model is valid
     def valid?
       warn '[DEPRECATED] the `valid?` method is obsolete'
-      return false if @state.nil?
-      state_validator = EnumAttributeValidator.new('String', ["creating", "restoring", "destroyed", "destroying", "started", "stopped", "starting", "stopping", "error", "build_failed", "pending_build", "building_snapshot", "unknown", "pulling_snapshot", "archived", "archiving", "resizing"])
-      return false unless state_validator.valid?(@state)
+      return false if !@cpu.nil? && @cpu < 1
+      return false if !@memory.nil? && @memory < 1
+      return false if !@disk.nil? && @disk < 1
       true
     end
 
-    # Custom attribute writer method checking allowed values (enum).
-    # @param [Object] state Object to be assigned
-    def state=(state)
-      validator = EnumAttributeValidator.new('String', ["creating", "restoring", "destroyed", "destroying", "started", "stopped", "starting", "stopping", "error", "build_failed", "pending_build", "building_snapshot", "unknown", "pulling_snapshot", "archived", "archiving", "resizing"])
-      unless validator.valid?(state)
-        fail ArgumentError, "invalid value for \"state\", must be one of #{validator.allowable_values}."
+    # Custom attribute writer method with validation
+    # @param [Object] cpu Value to be assigned
+    def cpu=(cpu)
+      if cpu.nil?
+        fail ArgumentError, 'cpu cannot be nil'
       end
-      @state = state
+
+      if cpu < 1
+        fail ArgumentError, 'invalid value for "cpu", must be greater than or equal to 1.'
+      end
+
+      @cpu = cpu
+    end
+
+    # Custom attribute writer method with validation
+    # @param [Object] memory Value to be assigned
+    def memory=(memory)
+      if memory.nil?
+        fail ArgumentError, 'memory cannot be nil'
+      end
+
+      if memory < 1
+        fail ArgumentError, 'invalid value for "memory", must be greater than or equal to 1.'
+      end
+
+      @memory = memory
+    end
+
+    # Custom attribute writer method with validation
+    # @param [Object] disk Value to be assigned
+    def disk=(disk)
+      if disk.nil?
+        fail ArgumentError, 'disk cannot be nil'
+      end
+
+      if disk < 1
+        fail ArgumentError, 'invalid value for "disk", must be greater than or equal to 1.'
+      end
+
+      @disk = disk
     end
 
     # Checks equality by comparing each attribute.
@@ -148,9 +164,9 @@ module DaytonaApiClient
     def ==(o)
       return true if self.equal?(o)
       self.class == o.class &&
-          state == o.state &&
-          error_reason == o.error_reason &&
-          recoverable == o.recoverable
+          cpu == o.cpu &&
+          memory == o.memory &&
+          disk == o.disk
     end
 
     # @see the `==` method
@@ -162,7 +178,7 @@ module DaytonaApiClient
     # Calculates hash code according to all attributes.
     # @return [Integer] Hash code
     def hash
-      [state, error_reason, recoverable].hash
+      [cpu, memory, disk].hash
     end
 
     # Builds the object from hash
