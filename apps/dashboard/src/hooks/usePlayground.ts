18c8fb66a7bbfe21ae37fd6657a080e7b02d22c4
Mislav Ivanda <mislavivanda454@gmail.com>
2026-02-09 10:13:09 +0000

    chore: usePlayground error message update
    Signed-off-by: Mislav Ivanda <mislavivanda454@gmail.com>

--- apps/dashboard/src/hooks/usePlayground.ts
+++ apps/dashboard/src/hooks/usePlayground.ts
@@ -10,7 +10,7 @@ export function usePlayground() {
   const context = useContext(PlaygroundContext)
 
   if (!context) {
-    throw new Error('usePlayground must be used within a <Playground />')
+    throw new Error('usePlayground must be used within a <PlaygroundProvider />')
   }
 
   return context
