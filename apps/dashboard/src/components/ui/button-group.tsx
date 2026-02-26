980fd3a5b9fcc65128b5be3a35c4c3b59504cbeb
rpavlini <rpavlinic@daytona.io>
2026-02-26 08:50:51 +0000

    chore(dashboard): license
    Signed-off-by: rpavlini <rpavlinic@daytona.io>

--- apps/dashboard/src/components/ui/button-group.tsx
+++ apps/dashboard/src/components/ui/button-group.tsx
@@ -1,3 +1,8 @@
+/*
+ * Copyright Daytona Platforms Inc.
+ * SPDX-License-Identifier: AGPL-3.0
+ */
+
 import * as Slot from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
