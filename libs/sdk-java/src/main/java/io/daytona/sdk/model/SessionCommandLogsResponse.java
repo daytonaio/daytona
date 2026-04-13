92f8d433ae44e1f99e3939f881e2dd81fde687d9
Mirko Dzaja <mirko@daytona.io>
2026-04-13 11:40:16 +0000

    chore: remove a year from license headers
    Signed-off-by: Mirko Dzaja <mirko@daytona.io>

--- libs/sdk-java/src/main/java/io/daytona/sdk/model/SessionCommandLogsResponse.java
+++ libs/sdk-java/src/main/java/io/daytona/sdk/model/SessionCommandLogsResponse.java
@@ -1,3 +1,6 @@
+// Copyright Daytona Platforms Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 package io.daytona.sdk.model;
 
 public class SessionCommandLogsResponse extends io.daytona.toolbox.client.model.SessionCommandLogsResponse {
