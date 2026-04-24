1161aa853474e2ef23bd1d2d2e9183b6215341ed
Muhammad Hashmi <mhashmi@berkeley.edu>
2026-04-24 11:17:12 -0700

    fix(a11y): clarify atspi D-Bus warning
    Signed-off-by: Muhammad Hashmi <mhashmi@berkeley.edu>

--- libs/computer-use/pkg/computeruse/computeruse.go
+++ libs/computer-use/pkg/computeruse/computeruse.go
@@ -210,7 +210,7 @@ func (c *ComputerUse) initializeProcesses(homeDir string) {
 		log.Warnf("at-spi-bus-launcher not found in any known location; accessibility API will return 503 until the binary is installed")
 	} else {
 		if err := waitForSessionBus(dbusAddress, 5*time.Second); err != nil {
-			log.Warnf("session D-Bus is not ready for at-spi-bus-launcher; starting supervised process anyway so it can restart until the bus is available: %v", err)
+			log.Warnf("session D-Bus check failed for at-spi-bus-launcher; starting supervised process anyway so it can retry when D-Bus is usable: %v", err)
 		}
 		c.processes["atspi"] = &Process{
 			Name:        "atspi",
