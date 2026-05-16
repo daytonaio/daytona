9d3725f5716d856c9dd6906c6a473eba59696f47
Goran Draganić <goran@daytona.io>
2026-05-16 09:31:21 +0200

    perf: optimize daemon size
    Signed-off-by: Goran Draganić <goran@daytona.io>

---
+++ apps/daemon/pkg/toolbox/swagger_disabled.go
@@ -0,0 +1,10 @@
+// Copyright 2025 Daytona Platforms Inc.
+// SPDX-License-Identifier: AGPL-3.0
+
+//go:build noswagger
+
+package toolbox
+
+import "github.com/gin-gonic/gin"
+
+func registerSwagger(_ *gin.Engine) {}
