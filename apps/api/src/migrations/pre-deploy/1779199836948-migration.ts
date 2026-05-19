aa2819b3accdf35eca089c76087068710f65857c
Toma Puljak <toma.puljak@hotmail.com>
2026-05-19 14:33:40 +0000

    comment
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- apps/api/src/migrations/pre-deploy/1779199836948-migration.ts
+++ apps/api/src/migrations/pre-deploy/1779199836948-migration.ts
@@ -9,6 +9,7 @@ export class Migration1779199836948 implements MigrationInterface {
   name = 'Migration1779199836948'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    // Note: not using CONCURRENTLY + skipping transactions because of reverting issue: https://github.com/typeorm/typeorm/issues/9981
     await queryRunner.query(
       `CREATE UNIQUE INDEX "sandbox_usage_periods_one_open_period_per_sandbox_idx" ON "sandbox_usage_periods" ("sandboxId") WHERE "endAt" IS NULL`,
     )
